03f92b0f1fdda959d833ec634a83ec0330f81591
Nathaniel McCallum <npmccallum@redhat.com>
2019-02-28 13:32:40 -0500

    Remove fwapi

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
 extern crate codicon;
 extern crate endicon;
 
-pub mod certs;
-pub mod fwapi;
+pub mod sev;
+pub mod ca;
+
+use std::fmt::{Display, Formatter, Result};
+use std::io;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    Invalid(String),
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match *self {
+            Error::IoError(ref e) => e.fmt(f),
+            Error::Invalid(ref s) => write!(f, "invalid: {}", s),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error::IoError(e)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Params;
