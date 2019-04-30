c9b0685c8b1923f515d928afd68406ce3181ed6d
Nathaniel McCallum <npmccallum@redhat.com>
2019-04-30 15:11:06 -0400

    Fix session generation

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -35,27 +35,7 @@ impl std::convert::TryFrom<launch::Policy> for Session<Initialized> {
 }
 
 impl Session<Initialized> {
-    pub fn from_keys(policy: launch::Policy, tek: Vec<u8>, tik: Vec<u8>) -> Self {
-        Self {
-            tek: key::Key::new(tek),
-            tik: key::Key::new(tik),
-            data: Initialized,
-            policy,
-        }
-    }
-
-    pub fn start(&self, chain: certs::Chain) -> Result<launch::Start> {
-        use certs::*;
-
-        let pdh = chain.verify()?;
-        let (crt, prv) = sev::Certificate::generate(sev::Usage::PDH)?;
-
-        let mut nonce = [0u8; 16];
-        let mut iv = [0u8; 16];
-        rand::rand_bytes(&mut nonce)?;
-        rand::rand_bytes(&mut iv)?;
-
-        let z = key::Key::new(prv.derive(&pdh)?);
+    fn session(&self, nonce: [u8; 16], iv: [u8; 16], z: key::Key) -> Result<launch::Session> {
         let master = z.derive(16, &nonce, "sev-master-secret")?;
         let kek = master.derive(16, &[], "sev-kek")?;
         let kik = master.derive(16, &[], "sev-kik")?;
@@ -75,16 +55,31 @@ impl Session<Initialized> {
         let wmac = kik.mac(&wrap)?;
         let pmac = self.tik.mac(&self.policy.bytes())?;
 
+        Ok(launch::Session {
+            policy_mac: pmac,
+            wrap_mac: wmac,
+            wrap_tk: wrap,
+            wrap_iv: iv,
+            nonce,
+        })
+    }
+
+    pub fn start(&self, chain: certs::Chain) -> Result<launch::Start> {
+        use certs::*;
+
+        let pdh = chain.verify()?;
+        let (crt, prv) = sev::Certificate::generate(sev::Usage::PDH)?;
+
+        let z = key::Key::new(prv.derive(&pdh)?);
+        let mut nonce = [0u8; 16];
+        let mut iv = [0u8; 16];
+        rand::rand_bytes(&mut nonce)?;
+        rand::rand_bytes(&mut iv)?;
+
         Ok(launch::Start {
             policy: self.policy,
             cert: crt,
-            session: launch::Session {
-                policy_mac: pmac,
-                wrap_mac: wmac,
-                wrap_tk: wrap,
-                wrap_iv: iv,
-                nonce,
-            }
+            session: self.session(nonce, iv, z)?
         })
     }
 
@@ -148,5 +143,87 @@ impl Session<Verified> {
             header: launch::Header { flags, mac, iv },
             ciphertext,
         })
+    }
+}
+
+#[cfg(test)]
+mod initialized {
+    use crate::{Build, Version, launch, session::Session};
+    use super::*;
+
+    #[test]
+    fn session() {
+        let session = Session {
+            policy: launch::Policy::default(),
+            tek: key::Key::new(vec![0u8; 16]),
+            tik: key::Key::new(vec![0u8; 16]),
+            data: Initialized,
+        };
+
+        let launch = session.session([0u8; 16], [0u8; 16], key::Key::zeroed(16)).unwrap();
+
+        assert_eq!(launch.wrap_iv, [0u8; 16]);
+
+        assert_eq!(launch.nonce, [0u8; 16]);
+
+        assert_eq!(launch.wrap_tk, [
+            0x21, 0x37, 0xbc, 0x7f, 0x9b, 0xb8, 0xbd, 0x7c,
+            0x3e, 0x55, 0xa5, 0x76, 0xa1, 0x5d, 0x34, 0x54,
+            0xb3, 0x85, 0x6b, 0x8b, 0xa2, 0x7a, 0xfa, 0xdf,
+            0x46, 0xdc, 0xfe, 0xe9, 0xf0, 0x2c, 0x02, 0xc4,
+        ]);
+
+        assert_eq!(launch.wrap_mac, [
+            0x31, 0x76, 0xc0, 0x75, 0x27, 0x38, 0xbd, 0x9d,
+            0x5e, 0x86, 0x68, 0x95, 0x34, 0x02, 0x0f, 0x52,
+            0x8c, 0x08, 0x8f, 0x16, 0x23, 0x88, 0x26, 0xb0,
+            0x00, 0xb3, 0x27, 0xde, 0xe6, 0xae, 0xed, 0x7d,
+        ]);
+
+        assert_eq!(launch.policy_mac, [
+            0xaa, 0x78, 0x55, 0xe1, 0x38, 0x39, 0xdd, 0x76,
+            0x7c, 0xd5, 0xda, 0x7c, 0x1f, 0xf5, 0x03, 0x65,
+            0x40, 0xc9, 0x26, 0x4b, 0x7a, 0x80, 0x30, 0x29,
+            0x31, 0x5e, 0x55, 0x37, 0x52, 0x87, 0xb4, 0xaf,
+        ]);
+    }
+
+    #[test]
+    fn verify() {
+        let digest = [
+            0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
+            0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
+            0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
+            0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55
+        ];
+
+        let measurement = launch::Measurement {
+            measure: [
+                0x6f, 0xaa, 0xb2, 0xda, 0xae, 0x38, 0x9b, 0xcd,
+                0x34, 0x05, 0xa0, 0x5d, 0x6c, 0xaf, 0xe3, 0x3c,
+                0x04, 0x14, 0xf7, 0xbe, 0xdd, 0x0b, 0xae, 0x19,
+                0xba, 0x5f, 0x38, 0xb7, 0xfd, 0x16, 0x64, 0xea
+            ],
+            mnonce: [
+                0x4f, 0xbe, 0x0b, 0xed, 0xba, 0xd6, 0xc8, 0x6a,
+                0xe8, 0xf6, 0x89, 0x71, 0xd1, 0x03, 0xe5, 0x54
+            ],
+        };
+
+        let policy = launch::Policy {
+            flags: launch::PolicyFlags::default(),
+            minfw: Version(0, 0),
+        };
+
+        let tek = key::Key::new(vec![0u8; 16]);
+        let tik = key::Key::new(vec![
+            0x66, 0x32, 0x0d, 0xb7, 0x31, 0x58, 0xa3, 0x5a,
+            0x25, 0x5d, 0x05, 0x17, 0x58, 0xe9, 0x5e, 0xd4
+        ]);
+
+        let session = Session { policy, tek, tik, data: Initialized };
+        let build = Build(Version(0x00, 0x12), 0x0f);
+
+        session.verify(&digest, build, measurement).unwrap();
     }
 }
--- tests/session.rs
+++ tests/session.rs
@@ -1,7 +1,7 @@
 #![cfg(feature = "openssl")]
 
 mod initialized {
-    use ::sev::{Build, Version, launch, session::Session, certs::*};
+    use ::sev::{launch, session::Session, certs::*};
     use codicon::Decoder;
     use std::convert::*;
 
@@ -12,57 +12,25 @@ mod initialized {
 
     #[test]
     fn start() {
+        const ARK: &[u8] = include_bytes!("naples/ark.cert");
+        const ASK: &[u8] = include_bytes!("naples/ask.cert");
+        const CEK: &[u8] = include_bytes!("naples/cek.cert");
+        const OCA: &[u8] = include_bytes!("naples/oca.cert");
+        const PEK: &[u8] = include_bytes!("naples/pek.cert");
+        const PDH: &[u8] = include_bytes!("naples/pdh.cert");
+
         let session = Session::try_from(launch::Policy::default()).unwrap();
         session.start(Chain {
             ca: ca::Chain {
-                ark: ca::Certificate::decode(&mut &include_bytes!("naples/ark.cert")[..], ()).unwrap(),
-                ask: ca::Certificate::decode(&mut &include_bytes!("naples/ask.cert")[..], ()).unwrap(),
+                ark: ca::Certificate::decode(&mut &ARK[..], ()).unwrap(),
+                ask: ca::Certificate::decode(&mut &ASK[..], ()).unwrap(),
             },
             sev: sev::Chain {
-                cek: sev::Certificate::decode(&mut &include_bytes!("naples/cek.cert")[..], ()).unwrap(),
-                oca: sev::Certificate::decode(&mut &include_bytes!("naples/oca.cert")[..], ()).unwrap(),
-                pek: sev::Certificate::decode(&mut &include_bytes!("naples/pek.cert")[..], ()).unwrap(),
-                pdh: sev::Certificate::decode(&mut &include_bytes!("naples/pdh.cert")[..], ()).unwrap(),
+                cek: sev::Certificate::decode(&mut &CEK[..], ()).unwrap(),
+                oca: sev::Certificate::decode(&mut &OCA[..], ()).unwrap(),
+                pek: sev::Certificate::decode(&mut &PEK[..], ()).unwrap(),
+                pdh: sev::Certificate::decode(&mut &PDH[..], ()).unwrap(),
             },
         }).unwrap();
-    }
-
-    #[test]
-    fn verify() {
-        let digest = [
-            0xe3, 0xb0, 0xc4, 0x42, 0x98, 0xfc, 0x1c, 0x14,
-            0x9a, 0xfb, 0xf4, 0xc8, 0x99, 0x6f, 0xb9, 0x24,
-            0x27, 0xae, 0x41, 0xe4, 0x64, 0x9b, 0x93, 0x4c,
-            0xa4, 0x95, 0x99, 0x1b, 0x78, 0x52, 0xb8, 0x55
-        ];
-
-        let measurement = launch::Measurement {
-            measure: [
-                0x6f, 0xaa, 0xb2, 0xda, 0xae, 0x38, 0x9b, 0xcd,
-                0x34, 0x05, 0xa0, 0x5d, 0x6c, 0xaf, 0xe3, 0x3c,
-                0x04, 0x14, 0xf7, 0xbe, 0xdd, 0x0b, 0xae, 0x19,
-                0xba, 0x5f, 0x38, 0xb7, 0xfd, 0x16, 0x64, 0xea
-            ],
-            mnonce: [
-                0x4f, 0xbe, 0x0b, 0xed, 0xba, 0xd6, 0xc8, 0x6a,
-                0xe8, 0xf6, 0x89, 0x71, 0xd1, 0x03, 0xe5, 0x54
-            ],
-        };
-
-        let policy = launch::Policy {
-            flags: launch::PolicyFlags::default(),
-            minfw: Version(0, 0),
-        };
-
-        let tek = vec![0u8; 16];
-        let tik = vec![
-            0x66, 0x32, 0x0d, 0xb7, 0x31, 0x58, 0xa3, 0x5a,
-            0x25, 0x5d, 0x05, 0x17, 0x58, 0xe9, 0x5e, 0xd4
-        ];
-
-        let session = Session::from_keys(policy, tek, tik);
-        let build = Build(Version(0x00, 0x12), 0x0f);
-
-        session.verify(&digest, build, measurement).unwrap();
     }
 }
