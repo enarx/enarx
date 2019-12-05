c6c9fe4955b72097c6278f8cbe9e1b27368abc3f
Nathaniel McCallum <npmccallum@redhat.com>
2019-12-05 16:53:52 +0000

    Remove alignment and padding from Tcs
    We will use the paged crate for this.

--- sgx-sys/src/tcs.rs
+++ sgx-sys/src/tcs.rs
@@ -1,10 +1,9 @@
-const SGX_TCS_RESERVED_SIZE: usize = 4024;
-
 /// Thread Control Structure (TCS) is an enclave page visible in its address
 /// space that defines an entry point inside the enclave. A thread enters inside
 /// an enclave by supplying address of TCS to ENCLU(EENTER). A TCS can be entered
 /// by only one thread at a time.
-#[repr(C, align(4096))]
+#[derive(Debug, Default)]
+#[repr(C)]
 pub struct Tcs {
     state: u64,         // used to mark an entered TCS
     flags: u64,         // execution flags (cleared by EADD)
@@ -17,24 +16,4 @@ pub struct Tcs {
     gs_offset: u64,     // offset relative to enclave base to become GS segment inside the enclave
     fs_limit: u32,      // size to become a new FS-limit (only 32-bit enclaves)
     gs_limit: u32,      // size to become a new GS-limit (only 32-bit enclaves)
-    reserved: [u8; SGX_TCS_RESERVED_SIZE],
-}
-
-impl Default for Tcs {
-    fn default() -> Self {
-        Tcs {
-            state: u64::default(),
-            flags: u64::default(),
-            ssa_offset: u64::default(),
-            ssa_index: u32::default(),
-            nr_ssa_frames: u32::default(),
-            entry_offset: u64::default(),
-            exit_addr: u64::default(),
-            fs_offset: u64::default(),
-            gs_offset: u64::default(),
-            fs_limit: u32::default(),
-            gs_limit: u32::default(),
-            reserved: [0u8; SGX_TCS_RESERVED_SIZE],
-        }
-    }
 }
