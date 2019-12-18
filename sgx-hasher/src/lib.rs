3a63104f962c0a6cc85f8ef55a31bee9152eea88
Nathaniel McCallum <npmccallum@redhat.com>
2019-12-18 15:12:54 +0000

    Add tests to the hasher
    First we made the hasher produce the same output as the Linux kernel SGX
    selftest. Then we made a bunch of tests to verify the algorithm doesn't
    accidentally change. These tests have not yet been tested against the Intel
    CPU, so they might still be incorrect in some corner cases. However, the
    algorithm should be fairly correct at this point.

--- sgx-hasher/src/lib.rs
+++ sgx-hasher/src/lib.rs
@@ -16,8 +16,6 @@
 #![allow(clippy::identity_op)]
 #![allow(clippy::unreadable_literal)]
 
-use std::io::Result;
-
 use openssl::sha;
 use sgx_types::{page, secs};
 
@@ -38,9 +36,9 @@ impl Hasher {
     }
 
     // Mimics call to SGX_IOC_ENCLAVE_ADD_PAGES (EADD and EEXTEND)
-    pub unsafe fn add(
+    pub fn add(
         &mut self,
-        offset: usize,
+        offset: u64,
         data: &[u8],
         flags: sgx_traits::Flags,
         secinfo: page::SecInfo,
@@ -48,36 +46,206 @@ impl Hasher {
         const EEXTEND: u64 = 0x00444E4554584545;
         const EADD: u64 = 0x0000000044444145;
 
-        let offset = offset as u64;
+        assert_eq!(data.len() % 4096, 0);
+        assert_eq!(offset % 4096, 0);
 
-        // Hash for the EADD instruction.
-        self.0.update(&EADD.to_le_bytes());
-        self.0.update(&(offset as u64).to_le_bytes());
-        self.0.update(&secinfo.as_ref()[..48]);
+        // For each page in the input...
+        for (i, page) in data.chunks(4096).enumerate() {
+            let off = offset + i as u64 * 4096;
 
-        // Hash for the EEXTEND instruction.
-        if let sgx_traits::Flags::MEASURE = flags {
-            for i in 0..16 {
-                let poff = i * 256;
-                let eoff = offset + poff as u64;
+            // Hash for the EADD instruction.
+            self.0.update(&EADD.to_le_bytes());
+            self.0.update(&off.to_le_bytes());
+            self.0.update(&secinfo.as_ref()[..48]);
 
-                self.0.update(&EEXTEND.to_le_bytes());
-                self.0.update(&eoff.to_le_bytes());
-                self.0.update(&[0u8; 48]);
+            // Hash for the EEXTEND instruction.
+            if flags.contains(sgx_traits::Flags::MEASURE) {
+                for (j, segment) in page.chunks(256).enumerate() {
+                    let off = off + j as u64 * 256;
 
-                self.0.update(&data[poff..][..256]);
+                    self.0.update(&EEXTEND.to_le_bytes());
+                    self.0.update(&off.to_le_bytes());
+                    self.0.update(&[0u8; 48]);
+                    self.0.update(segment);
+                }
             }
         }
     }
 
     // Produces MRENCLAVE value (SHA256 of fields specified in
     // https://github.com/enarx/enarx/wiki/SGX-Measurement)
-    pub fn finish(self) -> Result<[u8; 32]> {
-        Ok(self.0.finish())
+    pub fn finish(self) -> [u8; 32] {
+        self.0.finish()
     }
 }
 
 #[cfg(test)]
 mod test {
-    // TODO: Add tests (ex. for modulus, exponent of rsa key)
+    use super::*;
+    use sgx_types::page::{Flags as Perms, SecInfo};
+    use sgx_types::{attr, misc, sig};
+    use std::fs::File;
+    use std::io::Read;
+
+    // A NOTE ABOUT THIS TESTING METHODOLOGY
+    //
+    // The ANSWER values in this test are not guaranteed to be correct.
+    // They were produced by getting the hasher to output the same hash
+    // for the binary from the Linux kernel SGX selftest and then running
+    // the algorithm again to produce these ANSWERs. At least some of the
+    // ANSWERs are correct. The remaining ones alert us to algorithmic
+    // changes. We expect that over time our ANSWERs will be correct.
+    //
+    // The canonical source of correctness for this algorithm is, of
+    // course, the Intel SGX CPU/microcode/ME. If you can demonstrate a
+    // a case where we don't match this, we will happily change our ANSWERs.
+
+    const DATA: [u8; 4096] = [123u8; 4096];
+    const LONG: [u8; 8192] = [123u8; 8192];
+
+    fn hash(input: &[(&[u8], SecInfo)]) -> [u8; 32] {
+        // Add the lengths of all the enclave segments to produce enclave size.
+        let size = input.iter().fold(0, |c, x| c + x.0.len() as u64);
+
+        let contents = sig::Contents::new(
+            misc::MiscSelect::default(),
+            misc::MiscSelect::default(),
+            attr::Attributes::default(),
+            attr::Attributes::default(),
+            [0u8; 32],
+            0,
+            0,
+        );
+
+        // This creates an enclave with:
+        //     base address: 0
+        //     enclave size: the next power of two beyond our segments
+        //   ssa frame size: 1
+        //         contents: defined above
+        let secs = secs::Secs::new(0, size.next_power_of_two(), 1, &contents);
+        let mut hasher = Hasher::new(secs);
+
+        let mut off = 0;
+        for i in input {
+            hasher.add(off, i.0, sgx_traits::Flags::MEASURE, i.1);
+            off += i.0.len() as u64;
+        }
+
+        hasher.finish()
+    }
+
+    #[test]
+    fn empty() {
+        const ANSWER: [u8; 32] = [
+            252, 149, 215, 52, 58, 111, 14, 95, 207, 19, 57, 38, 97, 120, 23, 26, 207, 44, 152, 5,
+            72, 202, 97, 25, 204, 94, 10, 197, 188, 89, 246, 246,
+        ];
+        let question = hash(&[]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn tcs() {
+        const ANSWER: [u8; 32] = [
+            230, 83, 134, 171, 179, 130, 94, 239, 114, 13, 202, 111, 173, 126, 101, 185, 44, 96,
+            129, 56, 92, 7, 246, 99, 17, 85, 245, 207, 201, 9, 51, 65,
+        ];
+        let question = hash(&[(&DATA, SecInfo::tcs())]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn r() {
+        const ANSWER: [u8; 32] = [
+            0, 117, 112, 212, 9, 215, 100, 12, 99, 30, 102, 236, 187, 103, 39, 144, 251, 33, 191,
+            112, 25, 95, 140, 251, 201, 209, 113, 187, 15, 71, 15, 242,
+        ];
+        let question = hash(&[(&DATA, SecInfo::reg(Perms::R))]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn rw() {
+        const ANSWER: [u8; 32] = [
+            129, 184, 53, 91, 133, 145, 39, 205, 176, 182, 220, 37, 36, 198, 139, 91, 148, 181, 98,
+            116, 22, 122, 174, 173, 173, 59, 39, 209, 165, 47, 8, 219,
+        ];
+        let question = hash(&[(&DATA, SecInfo::reg(Perms::R | Perms::W))]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn rwx() {
+        const ANSWER: [u8; 32] = [
+            175, 209, 233, 45, 48, 189, 118, 146, 139, 110, 63, 192, 56, 119, 66, 69, 246, 116,
+            142, 206, 58, 97, 186, 173, 59, 110, 122, 19, 171, 237, 80, 6,
+        ];
+        let question = hash(&[(&DATA, SecInfo::reg(Perms::R | Perms::W | Perms::X))]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn rx() {
+        const ANSWER: [u8; 32] = [
+            76, 207, 169, 240, 107, 1, 166, 236, 108, 53, 91, 107, 135, 238, 123, 132, 35, 246,
+            230, 31, 254, 6, 3, 175, 35, 2, 39, 175, 114, 254, 73, 55,
+        ];
+        let question = hash(&[(&DATA, SecInfo::reg(Perms::R | Perms::X))]);
+        assert_eq!(question, ANSWER);
+    }
+
+    #[test]
+    fn long() {
+        const ANSWER: [u8; 32] = [
+            233, 11, 17, 35, 117, 163, 196, 106, 142, 137, 169, 130, 108, 108, 51, 5, 29, 241, 152,
+            190, 9, 245, 27, 16, 85, 173, 17, 90, 43, 124, 46, 84,
+        ];
+        let question = hash(&[(&DATA, SecInfo::tcs()), (&LONG, SecInfo::reg(Perms::R))]);
+        assert_eq!(question, ANSWER);
+    }
+
+    fn load(path: &str) -> Vec<u8> {
+        let mut file = File::open(path).unwrap();
+        let size = file.metadata().unwrap().len();
+
+        let mut data = vec![0u8; size as usize];
+        file.read_exact(&mut data).unwrap();
+
+        data
+    }
+
+    fn loadsig(path: &str) -> sig::Signature {
+        let mut sig: sig::Signature;
+        let buf: &mut [u8];
+
+        unsafe {
+            sig = std::mem::MaybeUninit::uninit().assume_init();
+            buf = std::slice::from_raw_parts_mut(
+                &mut sig as *mut _ as *mut u8,
+                std::mem::size_of_val(&sig),
+            );
+        }
+
+        let mut file = File::open(path).unwrap();
+        file.read_exact(buf).unwrap();
+
+        sig
+    }
+
+    #[test]
+    fn selftest() {
+        let bin = load("tests/encl.bin");
+        let sig = loadsig("tests/encl.ss");
+
+        let contents = sig.contents();
+
+        // Validate the hash.
+        assert_eq!(
+            contents.mrenclave(),
+            hash(&[
+                (&bin[..4096], SecInfo::tcs()),
+                (&bin[4096..], SecInfo::reg(Perms::R | Perms::W | Perms::X))
+            ])
+        );
+    }
 }
