58fd94bf66571a10222386b3a2d52cab6126deb0
Lily Sturmann <lsturman@redhat.com>
2020-01-16 21:04:29 +0000

    Add General Protection Fault case in sgx-types/src/ssa.rs

--- sgx-types/src/ssa.rs
+++ sgx-types/src/ssa.rs
@@ -179,6 +179,7 @@ pub struct Gpr {
 }
 
 bitflags! {
+    /// Flags for a page fault;
     /// Section 38.9.2.2, Table 38-13
     pub struct PageFault: u32 {
         /// Same as non-SGX page fault exception P flag.
@@ -192,19 +193,72 @@ bitflags! {
         /// Protection Key induced fault.
         const PK = 1 << 5;
         /// EPCM induced fault.
-        const SGX = 1 << 5;
+        const SGX = 1 << 15;
     }
+}
+
+bitflags! {
+    /// Flags for a general protection fault;
+    /// The General Protection Fault sets an error code, which is the segment selector index
+    /// when the exception is segment related. Otherwise, 0.
+    /// For more, refer to: https://wiki.osdev.org/Exceptions
+    pub struct GenProtFault: u32 {
+        ///  Exception originated externally to the processor.
+        const E = 1 << 0;
+        /// Bits 1 and 2 together indicate whether the selector (bits 3-15) references a
+        /// descriptor in the GDT, IDT, or LDT.
+        const TBL1 = 1 << 1;
+        /// Bits 1 and 2 together indicate whether the selector (bits 3-15) references a
+        /// descriptor in the GDT, IDT, or LDT.
+        const TBL2 = 1 << 2;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL3 = 1 << 3;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL4 = 1 << 4;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL5 = 1 << 5;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL6 = 1 << 6;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL7 = 1 << 7;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL8 = 1 << 8;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL9 = 1 << 9;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL10 = 1 << 10;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL11 = 1 << 11;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL12 = 1 << 12;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL13 = 1 << 13;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL14 = 1 << 14;
+        /// Selector index (bits 3-15) for the GDT, IDT, or LDT.
+        const SEL15 = 1 << 15;
+    }
+}
+
+/// The `errcd` in ExceptionInfo could be a page fault or a general protection fault.
+/// This type represents either.
+#[derive(Debug)]
+pub enum Fault {
+    /// General Protection Fault with flags.
+    GP(GenProtFault),
+    /// Page Fault with flags and memory address.
+    PF(u64, PageFault),
 }
 
 /// Section 38.9.2.1, Table 38-12
 #[derive(Debug)]
 #[repr(C)]
-pub struct ExceptionInfo {
+struct ExceptionInfo {
     /// In case of a page fault, contains the linear address that caused the fault.
-    pub maddr: u64,
+    maddr: u64,
     /// Exception error code for GP fault or page fault.
-    /// TODO: Should this have more options in case of general purpose fault?
-    pub errcd: PageFault,
+    errcd: u32,
+    reserved: u32,
 }
 
 /// Section 38.9.2, Table 38-11
@@ -213,7 +267,7 @@ pub struct ExceptionInfo {
 pub struct Miscellaneous {
     /// Exception info for GP or page fault occurring inside an enclave can be written to
     /// this struct under some conditions (see Table 38.11).
-    pub exinfo: ExceptionInfo,
+    exinfo: ExceptionInfo,
 }
 
 /// The size of XSAVE region in SSA is derived from the enclave’s support of the collection
@@ -252,6 +306,21 @@ impl Footer {
             padding1: [0; 14],
             misc,
             gpr,
+        }
+    }
+
+    /// Converts an internal fault type to a safe Rust enum describing the fault.
+    pub fn fault(&self) -> Option<Fault> {
+        self.gpr.exitinfo.exit_type()?;
+        match self.gpr.exitinfo.exception() {
+            Some(Exception::PageFault) => Some(Fault::PF(
+                self.misc.exinfo.maddr,
+                PageFault::from_bits(self.misc.exinfo.errcd).unwrap(),
+            )),
+            Some(Exception::GeneralProtection) => Some(Fault::GP(
+                GenProtFault::from_bits(self.misc.exinfo.errcd).unwrap(),
+            )),
+            _ => None,
         }
     }
 }
@@ -302,7 +371,8 @@ testaso! {
 
     struct ExceptionInfo: 8, 16 => {
         maddr: 0,
-        errcd: 8
+        errcd: 8,
+        reserved: 12
     }
 
     struct Miscellaneous: 8, 16 => {
