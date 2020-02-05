bdb7cb5d027b16dee319fe8569d542d28e125892
Lily Sturmann <lsturman@redhat.com>
2020-02-05 20:59:48 +0000

    Add XSave-related types to intel-types
    These types are based on
    https://github.com/jsakkine-intel/linux-sgx/blob/master/tools/testing/selftests/x86/sgx/encl_bootstrap.S#L83
    and Intel's 64-ia-32 Architectures Software Developer Manual, Vol. 1
    (link in intel-types/src/lib.rs).
    
    This commit also temporarily removes the Debug trait on SSA;
    this is an issue for which a ticket has been filed.

--- intel-types/src/lib.rs
+++ intel-types/src/lib.rs
@@ -9,7 +9,9 @@
 #![allow(clippy::identity_op)]
 #![deny(missing_docs)]
 
+use bitflags::bitflags;
 use core::{fmt::Debug, ops::BitAnd};
+use testing::testaso;
 
 /// Succinctly describes a masked type, e.g. masked Attributes or masked MiscSelect.
 /// A mask is applied to Attributes and MiscSelect structs in a Signature (SIGSTRUCT)
@@ -40,12 +42,313 @@ impl<T: Copy + Debug + PartialEq + BitAnd<Output = T>> PartialEq<T> for Masked<T
     }
 }
 
-/// The size of XSAVE region in SSA is derived from the enclave’s support of the collection
-/// of processor extended states that would be managed by XSAVE. The enablement of those
-/// processor extended state components in conjunction with CPUID leaf 0DH information
-/// determines the XSAVE region size in SSA.
-///
-/// Section 38.9, Table 38-7
+/// This type represents an MMX register, used to perform operations on 64-bit packed integer data.
+/// It includes the 10-byte value as well as 6 bytes of padding.
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct Mm(u128);
+
+/// This type represents an XMM 128-bit data register, used to operate on packed single-
+/// precision floating-point operands.
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct Xmm(u128);
+
+bitflags! {
+    /// x87 Floating Point Unit (FPU) Control Word
+    /// Section 8.1.5
+    pub struct Fcw: u16 {
+        /// Invalid Operation
+        const INV_OP = 1 << 0;
+
+        /// Denormal Operand
+        const DENORM_OP = 1 << 1;
+
+        /// Zero Divide
+        const ZERO_DIV = 1 << 2;
+
+        /// Overflow
+        const OVERFLOW = 1 << 3;
+
+        /// Underflow
+        const UNDERFLOW = 1 << 4;
+
+        /// Precision
+        const PREC = 1 << 5;
+
+        /// Precision Control 0
+        const PREC_CTRL0 = 1 << 8;
+
+        /// Precision Control 1
+        const PREC_CTRL1 = 1 << 9;
+
+        /// Rounding Control 0
+        const ROUND_CTRL0 = 1 << 10;
+
+        /// Rounding Control 1
+        const ROUND_CTRL1 = 1 << 11;
+
+        /// Infinity Control
+        const INFINITY_CTRL = 1 << 12;
+    }
+}
+
+impl Default for Fcw {
+    /// The x87 state initial configuration for FCW masks all floating-point exceptions,
+    /// sets rounding to nearest, and sets the x87 FPU precision to 64 bits.
+    /// See Section 8.1.5.
+    fn default() -> Self {
+        Fcw::INV_OP
+            | Fcw::DENORM_OP
+            | Fcw::ZERO_DIV
+            | Fcw::OVERFLOW
+            | Fcw::UNDERFLOW
+            | Fcw::PREC
+            | Fcw::PREC_CTRL0
+            | Fcw::PREC_CTRL1
+    }
+}
+
+bitflags! {
+    /// 32-bit register providing status and control bits used in SIMD floating-point operations
+    pub struct MxCsr: u32 {
+        /// Invalid Operation Flag
+        const INV_OP = 1 << 0;
+
+        /// Denormal Flag
+        const DENORM = 1 << 1;
+
+        /// Divide by Zero Flag
+        const ZERO_DIV = 1 << 2;
+
+        /// Overflow Flag
+        const OVERFLOW = 1 << 3;
+
+        /// Underflow Flag
+        const UNDERFLOW = 1 << 4;
+
+        /// Precision Flag
+        const PREC = 1 << 5;
+
+        /// Denormals are Zeros
+        const DENORM_ARE_ZEROS = 1 << 6;
+
+        /// Invalid Operation Mask
+        const INV_OP_MASK = 1 << 7;
+
+        /// Denormal Operation Mask
+        const DENORM_MASK = 1 << 8;
+
+        /// Divide by Zero Mask
+        const ZERO_DIV_MASK = 1 << 9;
+
+        /// Overflow Mask
+        const OVERFLOW_MASK = 1 << 10;
+
+        /// Underflow Mask
+        const UNDERFLOW_MASK = 1 << 11;
+
+        /// Precision Mask
+        const PREC_MASK = 1 << 12;
+
+        /// Rounding Control 0
+        const ROUND_CTRL0 = 1 << 13;
+
+        /// Rounding Control 1
+        const ROUND_CTRL1 = 1 << 14;
+
+        /// Flush to Zero
+        const FLUSH_TO_ZERO = 1 << 15;
+    }
+}
+
+impl Default for MxCsr {
+    /// The initial state of MXCSR after power-up/reset or INIT; mask allows software
+    /// to identify any reserved bits in MXCSR (none are reserved here).
+    /// See Sections 11.6.4 and 11.6.6.
+    fn default() -> Self {
+        MxCsr::INV_OP_MASK
+            | MxCsr::DENORM_MASK
+            | MxCsr::ZERO_DIV_MASK
+            | MxCsr::OVERFLOW_MASK
+            | MxCsr::UNDERFLOW_MASK
+            | MxCsr::PREC_MASK
+    }
+}
+
+impl Default for Masked<MxCsr> {
+    fn default() -> Self {
+        Self {
+            data: MxCsr::default(),
+            mask: MxCsr::all(),
+        }
+    }
+}
+
+bitflags! {
+    /// Section 13.4.3
+    pub struct XcompBv: u64 {
+        /// Compacted form is used for the layout of the XSAVE EXtended Region
+        const COMPACT = 1 << 63;
+    }
+}
+
+impl Default for XcompBv {
+    /// XCOMP_BV[63] = 1, compaction mode for the XSave Extended Region. However, no state
+    /// components are included in the Extended Region as all other bits are 0. The size of
+    /// the XSaveXtd is therefore zero.
+    // TODO: Check this value, as the original appears to set bit 31 but claims to set bit 63
+    fn default() -> Self {
+        XcompBv::COMPACT
+    }
+}
+
+bitflags! {
+    /// The state component bitmap identifies the state components present in the XSAVE area.
+    /// Bits 62:10 are reserved. Bit 63 does not correspond to any state component.
+    /// Section 13.1
+    #[derive(Default)]
+    pub struct XstateBv: u64 {
+        /// x87 state (Section 13.5.1)
+        const X87 = 1 << 0;
+
+        /// SSE state (Section 13.5.2)
+        const SSE = 1 << 1;
+
+        /// AVX state (Section 13.5.3)
+        const AVX = 1 << 2;
+
+        /// MPX state: BND0-BND3 (BNDREGS state)
+        const BNDREGS = 1 << 3;
+
+        /// MPX state: BNDCFGU and BNDSTATUS (BNDCSR state)
+        const BNDCSR  = 1 << 4;
+
+        /// AVX-512 state: opmask state
+        const AVX512_OPMASK = 1 << 5;
+
+        /// AVX-512 state: ZMM_HI256 state
+        const AVX512_ZMM_HI256 = 1 << 6;
+
+        /// AVX-512 state: HI16_ZMM state
+        const AVX512_HI16_ZMM = 1 << 7;
+
+        /// Processor Trace MSRs
+        const PT = 1 << 8;
+
+        /// Protection key feature register (Section 13.5.7)
+        const PKRU = 1 << 9;
+    }
+}
+
+/// The legacy region of an XSAVE area comprises the 512 bytes starting at the area's base address.
+/// See Table 13-1. There is no alignment requirement.
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct XSaveLegacy {
+    /// x87 Floating Point Unit (FPU) Control Word
+    pub fcw: Fcw,
+
+    /// x87 FPU Status Word
+    pub fsw: u16,
+
+    /// x87 FPU Tag Word
+    pub ftw: u8,
+
+    /// Reserved
+    pub reserved0: u8,
+
+    /// x87 FPU Opcode
+    pub fop: u16,
+
+    /// x87 FPU Instruction Pointer Offset
+    pub fip: u64,
+
+    /// x87 FPU Data Pointer Offset
+    pub fdp: u64,
+
+    /// 32-bit register providing status and control bits used in SIMD floating-point operations
+    pub mxcsr: Masked<MxCsr>,
+
+    /// Register used to perform operations on 64-bit packed integer data
+    pub mm: [Mm; 8],
+
+    /// 128-bit data register used to operate on packed single-precision floating-point operands
+    pub xmm: [Xmm; 16],
+
+    /// Padding: Size of XSaveLegacy must be 512.
+    pub padding0: [u64; 11],
+
+    /// Padding: Size of XSaveLegacy must be 512.
+    pub padding1: [u8; 7],
+}
+
+/// The XSAVE header of an XSAVE area comprises the 64 bytes starting at offset 512 from the
+/// area's base address. See Section 13.4.2. There is no alignment requirement.
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct XSaveHeader {
+    /// State-component bitmap identifying the state components in the XSAVE area.
+    pub xstate_bv: XstateBv,
+
+    /// State-component bitmap indicating the format of the XSAVE extended region and whether
+    /// a component is in the XSAVE area.
+    pub xcomp_bv: XcompBv,
+
+    /// Reserved
+    pub reserved0: [u64; 6],
+}
+
+/// For our use case, the XSave Extended Region is in compacted format and currently holds nothing,
+/// as specifed in XCOMP_BV in
+/// https://github.com/jsakkine-intel/linux-sgx/blob/master/tools/testing/selftests/x86/sgx/encl_bootstrap.S#L89.
+/// For more on this region, see Section 13.4.3. The alignment requirements are variable and specified in
+/// 13.4.3.
+#[derive(Debug, Default)]
+#[repr(C)]
+pub struct XSaveExtend;
+
+/// For details on the fields included in XSave, see Section 13.4. Must be 64 byte aligned.
 #[derive(Debug)]
-#[repr(C, align(4096))]
-pub struct XSave([[u64; 32]; 16]);
+#[repr(C, align(64))]
+pub struct XSave {
+    /// Legacy region of the XSave area
+    pub legacy: XSaveLegacy,
+
+    /// XSave header
+    pub header: XSaveHeader,
+
+    /// XSave Extended Region (not used)
+    pub extend: XSaveExtend,
+}
+
+testaso! {
+    struct XSaveLegacy: 8, 512 => {
+        fcw: 0,
+        fsw: 2,
+        ftw: 4,
+        reserved0: 5,
+        fop: 6,
+        fip: 8,
+        fdp: 16,
+        mxcsr: 24,
+        mm: 32,
+        xmm: 160,
+        padding0: 416,
+        padding1: 504
+    }
+
+    struct XSaveHeader: 8, 64 => {
+        xstate_bv: 0,
+        xcomp_bv: 8,
+        reserved0: 16
+    }
+
+    struct XSaveExtend: 1, 0 => { }
+
+    struct XSave: 64, 576 => {
+        legacy: 0,
+        header: 512,
+        extend: 576
+    }
+}
