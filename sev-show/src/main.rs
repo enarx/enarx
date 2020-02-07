e0c108713a4b8727770e1d0f23932a9a63e65c60
Connor Kuehl <ckuehl@redhat.com>
2020-02-07 00:04:35 +0000

    Create sev-show crate

---
+++ sev-show/src/main.rs
@@ -0,0 +1,11 @@
+//! This program performs various checks in the running system to
+//! discover hardware and kernel support for AMD Secure Encrypted
+//! Virtualization (SEV) technology. Like its counterpart, `sgx-show`,
+//! this program will create a tree-like hierarchy of tests to look
+//! for AMD SEV capabilities.
+//!
+//! Note: this will eventually be superseded by the consolidation
+//! of `sgx-show`-like utilities.
+
+fn main() {
+}
