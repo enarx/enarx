2aa81661393c5c55696f0f34e8245cb67788f8a4
Connor Kuehl <ckuehl@redhat.com>
2020-02-07 00:33:13 +0000

    Add LICENSE

--- sev-show/src/main.rs
+++ sev-show/src/main.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 //! This program performs various checks in the running system to
 //! discover hardware and kernel support for AMD Secure Encrypted
 //! Virtualization (SEV) technology. Like its counterpart, `sgx-show`,
