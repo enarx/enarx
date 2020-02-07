1fa434461798555275b8f3985ebedca1e867ec16
Connor Kuehl <ckuehl@redhat.com>
2020-02-07 00:07:09 +0000

    Add framework for probing SEV-capabilities

--- sev-show/src/main.rs
+++ sev-show/src/main.rs
@@ -9,5 +9,38 @@
 //! Note: this will eventually be superseded by the consolidation
 //! of `sgx-show`-like utilities.
 
+mod show;
+
+use show::*;
+
+/// Emits the results described in `tests` and prints them in a
+/// tree-like fashion.
+fn emit_results(tests: Vec<CompletedTest>, indent: usize) {
+    use colored::*;
+
+    for test in tests {
+        let icon = if test.passed() {
+            "✔".green()
+        } else {
+            "✗".red()
+        };
+        let info = test.info.clone().unwrap_or("".to_string());
+
+        println!("{:>space$}{} {}{}", "", icon, test, info, space = indent);
+        if let Some(dependents) = test.dependents {
+            emit_results(dependents, indent + 2);
+        }
+    }
+}
+
 fn main() {
+    let tests = vec![Test {
+        name: "Stub",
+        func: Box::new(|| (Ok(()), None)),
+        dependents: vec![],
+    }];
+
+    let completed = tests.into_iter().map(|t| t.run()).collect();
+
+    emit_results(completed, 0);
 }
