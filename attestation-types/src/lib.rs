cb86e5f20851ec6ff78207f8f05bb20cfccd5db0
Lily Sturmann <lsturman@redhat.com>
2020-02-12 20:21:45 +0000

    Add attestation-types crate
    Move Report and Targetinfo types from sgx-types to attestation-types.

--- attestation-types/src/lib.rs
+++ attestation-types/src/lib.rs
@@ -4,16 +4,9 @@
 //! Section references in further documentation refer to this document.
 //! https://www.intel.com/content/dam/www/public/emea/xe/en/documents/manuals/64-ia-32-architectures-software-developer-vol-3d-part-4-manual.pdf
 
-#![no_std]
 #![deny(clippy::all)]
 #![allow(clippy::identity_op)]
 #![deny(missing_docs)]
 
-pub mod attr;
-pub mod isv;
-pub mod misc;
-pub mod page;
-pub mod secs;
-pub mod sig;
-pub mod ssa;
-pub mod tcs;
+pub mod report;
+pub mod ti;
