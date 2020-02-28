d1c7609fbf3a76ee7dd11bd162f8b41e6eb50195
Nathaniel McCallum <npmccallum@redhat.com>
2020-02-28 13:30:18 -0500

    Reimplement CpuId
    We store the results as CpuidResult instead of u128. This allows us to
    implement `Debug` which helps with debugging.

--- sgx-show/src/data/cpuid.rs
+++ sgx-show/src/data/cpuid.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use core::arch::x86_64::__cpuid_count;
+use core::arch::x86_64::{CpuidResult, __cpuid_count};
 use core::marker::PhantomData;
 
 macro_rules! mkcpuid {
@@ -34,31 +34,52 @@ pub trait Leaf: std::fmt::Display {
     const SUBL: u32;
 }
 
-pub struct CpuId<T: Leaf>(pub u128, PhantomData<T>);
+pub struct CpuId<T: Leaf>(CpuidResult, PhantomData<T>);
 
-#[allow(clippy::identity_op)]
-#[allow(clippy::erasing_op)]
+impl<T: Leaf> std::fmt::Debug for CpuId<T> {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        fmt.debug_struct("CpuId")
+            .field("eax", &self.0.eax)
+            .field("ebx", &self.0.ebx)
+            .field("ecx", &self.0.ecx)
+            .field("edx", &self.0.edx)
+            .finish()
+    }
+}
+
 impl<T: Leaf> CpuId<T> {
     pub fn eax(&self) -> u32 {
-        (self.0 >> (3 * 32)) as u32
+        self.0.eax
     }
 
     pub fn ebx(&self) -> u32 {
-        (self.0 >> (2 * 32)) as u32
+        self.0.ebx
     }
 
     pub fn ecx(&self) -> u32 {
-        (self.0 >> (1 * 32)) as u32
+        self.0.ecx
     }
 
     pub fn edx(&self) -> u32 {
-        (self.0 >> (0 * 32)) as u32
+        self.0.edx
     }
 }
 
 impl<T: Leaf> From<CpuId<T>> for u128 {
     fn from(value: CpuId<T>) -> Self {
-        value.0
+        let mut out = 0;
+        out |= value.0.eax as Self;
+
+        out <<= 32;
+        out |= value.0.ebx as Self;
+
+        out <<= 32;
+        out |= value.0.ecx as Self;
+
+        out <<= 32;
+        out |= value.0.edx as Self;
+
+        out
     }
 }
 
@@ -66,19 +87,9 @@ impl<T: Leaf> super::Data for T {
     type Type = CpuId<T>;
 
     fn data(&self) -> Option<Self::Type> {
-        let res = unsafe { __cpuid_count(Self::LEAF, Self::SUBL) };
-
-        let mut out = res.eax as u128;
-
-        out <<= 32;
-        out |= res.ebx as u128;
-
-        out <<= 32;
-        out |= res.ecx as u128;
-
-        out <<= 32;
-        out |= res.edx as u128;
-
-        Some(CpuId(out, PhantomData))
+        Some(CpuId(
+            unsafe { __cpuid_count(Self::LEAF, Self::SUBL) },
+            PhantomData,
+        ))
     }
 }
