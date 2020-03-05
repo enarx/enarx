d9e9bcd8899837bca87e270f79fead018688a9c1
Nathaniel McCallum <npmccallum@redhat.com>
2020-03-04 23:31:55 -0500

    Add a Reader example

---
+++ crt0stack/examples/reader.rs
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use crt0stack::Reader;
+
+fn main() {
+    extern "C" {
+        static environ: *const *const std::os::raw::c_char;
+    }
+
+    let reader = unsafe {
+        let mut ptr = environ as *const usize;
+        ptr = ptr.sub(1);
+        assert_eq!(*ptr, 0);
+        ptr = ptr.sub(1);
+
+        let mut len = 0;
+        while *ptr != len {
+            ptr = ptr.sub(1);
+            len += 1;
+        }
+
+        Reader::new(&*(ptr as *const ()))
+    };
+
+    assert_eq!(reader.count(), 1);
+
+    let mut reader = reader.done();
+    for arg in &mut reader {
+        eprintln!("arg: {:?}", arg);
+    }
+
+    let mut reader = reader.done();
+    for env in &mut reader {
+        println!("env: {:?}", env);
+    }
+
+    let mut reader = reader.done();
+    for aux in &mut reader {
+        println!("aux: {:?}", aux);
+    }
+}
