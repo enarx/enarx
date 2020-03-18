cb59507741948c09ed66020e0d6da4931c110dcd
Nathaniel McCallum <npmccallum@redhat.com>
2020-03-18 15:16:54 -0400

    Update linux-syscall to use enumerate
    Also, generate constants with bindgen for greater accuracy.

--- linux-syscall/src/lib.rs
+++ linux-syscall/src/lib.rs
@@ -4,8 +4,17 @@
 #![no_std]
 #![deny(clippy::all)]
 
+// SysCall values generated with:
+//
+// ```
+// bindgen /usr/include/sys/syscall.h \
+//   | sed -rn 's|pub const SYS_([a-z][a-z0-9_]*): u32 = ([0-9]+);|\1 = \2,|p' \
+//   | tr [:lower:] [:upper:] \
+//   | sort -g -t= -k2
+// ```
+
 /// x86_64 syscalls
 pub mod x86_64;
 
 #[cfg(target_arch = "x86_64")]
-pub use x86_64::*;
+pub use x86_64::SysCall;
