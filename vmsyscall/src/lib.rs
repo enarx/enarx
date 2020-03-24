aa076ea57dfc5878bf67757fd7d384ec09565a91
Connor Kuehl <ckuehl@redhat.com>
2020-03-24 19:23:03 +0000

    Add common code that is shared between SEV keep and shim
    This will allow us to more easily parallelize our efforts to porting in
    Harald's work. There will be two projects consuming this common core:
    
    	- 'vmrun' AKA enarx-keep-sev
    	- 'kernel' AKA enarx-keep-sev-shim

--- vmsyscall/src/lib.rs
+++ vmsyscall/src/lib.rs
@@ -1,94 +1,92 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! syscall serialize/deserialize
+//! This crate is the interface between the SEV hypervisor (keep) and
+//! microkernel (shim). It enables system call proxying to the host
+//! and serialization of system calls.
 //!
 //! Currently it uses a hard coded page and an I/O trigger.
 //! We might want to switch to MMIO.
 
 #![deny(missing_docs)]
 #![deny(clippy::all)]
+#![deny(improper_ctypes)]
 #![no_std]
 
-use serde::{Deserialize, Serialize};
+pub mod bootinfo;
+pub mod memory_map;
 
-/// The syscalls to be serialized/deserialized via serde
-/// for the Hypervisor <-> VM syscall proxy
-#[derive(Serialize, Deserialize, Debug)]
+use core::fmt::{Debug, Formatter};
+
+impl Debug for VmSyscall {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            VmSyscall::Read { .. } => f.write_str("read(2)"),
+            VmSyscall::Write { .. } => f.write_str("write(2)"),
+            VmSyscall::Madvise { .. } => f.write_str("madvise(2)"),
+            VmSyscall::Mmap { .. } => f.write_str("mmap(2)"),
+            VmSyscall::Mremap { .. } => f.write_str("mremap(2)"),
+            VmSyscall::Munmap { .. } => f.write_str("munmap(2)"),
+            VmSyscall::Mprotect { .. } => f.write_str("mprotect(2)"),
+        }
+    }
+}
+
+/// maximum length of write(2) buffer
+pub const WRITE_BUF_LEN: usize = 4000;
+
+/// The syscalls for the Hypervisor <-> VM syscall proxy
+#[allow(clippy::large_enum_variant, missing_docs)]
 pub enum VmSyscall {
-    /// int madvise(void *addr, size_t length, int advice);
+    Read {
+        fd: u32,
+        count: usize,
+    },
+    Write {
+        fd: u32,
+        count: usize,
+        data: [u8; WRITE_BUF_LEN],
+    },
     Madvise {
-        /// see madvise(2)
         addr: usize,
-        /// see madvise(2)
         length: usize,
-        /// see madvise(2)
         advice: i32,
     },
-    /// void *mmap(void *addr, size_t length, int prot, int flags, …);
     Mmap {
-        /// see mmap(2)
         addr: usize,
-        /// see mmap(2)
         length: usize,
-        /// see mmap(2)
         prot: i32,
-        /// see mmap(2)
         flags: i32,
     },
-    /// void *mremap(void *old_address, size_t old_size, size_t new_size, int flags, ... /* void *new_address */);
     Mremap {
-        /// see mremap(2)
         old_address: usize,
-        /// see mremap(2)
         old_size: usize,
-        /// see mremap(2)
         new_size: usize,
-        /// see mremap(2)
         flags: i32,
     },
-    /// int munmap(void *addr, size_t length);
     Munmap {
-        /// see munmap(2)
         addr: usize,
-        /// see munmap(2)
         length: usize,
     },
-    /// int mprotect(void *addr, size_t len, int prot);
     Mprotect {
-        /// see mprotect(2)
         addr: usize,
-        /// see mprotect(2)
         length: usize,
-        /// see mprotect(2)
         prot: i32,
     },
     // Todo: extend with needed hypervisor proxy syscalls
 }
 
+/// A Linux ErrNo (see libc crate)
+pub type ErrNo = i32;
+
 /// The return value of the syscalls to be serialized/deserialized via serde
 /// for the Hypervisor <-> VM syscall proxy
-#[derive(Serialize, Deserialize, Debug)]
+#[allow(clippy::large_enum_variant, missing_docs)]
 pub enum VmSyscallRet {
-    /// int madvise(void *addr, size_t length, int advice);
-    Madvise(Result<i32, Error>),
-    /// void *mmap(void *addr, size_t length, int prot, int flags, …);
-    Mmap(Result<usize, Error>),
-    /// void *mremap(void *old_address, size_t old_size, size_t new_size, int flags, ... /* void *new_address */);
-    Mremap(Result<usize, Error>),
-    /// int munmap(void *addr, size_t length);
-    Munmap(Result<i32, Error>),
-    /// int mprotect(void *addr, size_t len, int prot);
-    Mprotect(Result<i32, Error>),
-}
-
-/// The error codes of the syscalls to be serialized/deserialized via serde
-/// for the Hypervisor <-> VM syscall proxy
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub enum Error {
-    /// standard error
-    Errno(i64),
-    /// serialize error
-    SerializeError,
-    /// deserialize error
-    DeSerializeError,
+    Read(Result<(i32, [u8; WRITE_BUF_LEN]), ErrNo>),
+    Write(Result<i32, ErrNo>),
+    Madvise(Result<i32, ErrNo>),
+    Mmap(Result<usize, ErrNo>),
+    Mremap(Result<usize, ErrNo>),
+    Munmap(Result<i32, ErrNo>),
+    Mprotect(Result<i32, ErrNo>),
 }
