0a4caab71f2b24af238de275424254161362e23b
Nathaniel McCallum <npmccallum@redhat.com>
2020-03-25 15:41:38 -0400

    Create a test payload crate that can be use for SGX and SEV

---
+++ payload/src/main.rs
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+
+extern "C" {
+    static environ: *const *const std::os::raw::c_char;
+}
+
+fn main() {
+    let reader = unsafe { crt0stack::Reader::from_environ(&*environ) }
+        .prev()
+        .prev();
+
+    let mut reader = reader.done();
+    for arg in &mut reader {
+        println!("arg: {:?}", arg);
+    }
+
+    let mut reader = reader.done();
+    for env in &mut reader {
+        println!("env: {:?}", env);
+    }
+
+    let mut reader = reader.done();
+    for aux in &mut reader {
+        println!("aux: {:?}", aux);
+    }
+}
