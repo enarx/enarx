3be7350f41c997621e41db464f2c6accb6f327cc
Nathaniel McCallum <npmccallum@redhat.com>
2020-04-03 09:21:34 -0400

    Move enclave creation logic into a separate function

--- enarx-keep-sgx/src/main.rs
+++ enarx-keep-sgx/src/main.rs
@@ -16,65 +16,67 @@ use span::Span;
 
 use enclave::Leaf;
 
-fn main() {
+fn load() -> (enclave::Enclave, usize) {
     const USAGE: &str = "Usage: enarx-keep-sgx <shim> <code>";
 
-    let (enclave, entry) = {
-        // Get the arguments.
-        let mut args = std::env::args();
-        let shim = args.nth(1).expect(USAGE);
-        let code = args.next().expect(USAGE);
+    // Get the arguments.
+    let mut args = std::env::args();
+    let shim = args.nth(1).expect(USAGE);
+    let code = args.next().expect(USAGE);
 
-        // Parse the shim and code and validate assumptions.
-        let shim = component::Component::from_path(shim).expect("Unable to parse shim");
-        let code = component::Component::from_path(code).expect("Unable to parse code");
+    // Parse the shim and code and validate assumptions.
+    let shim = component::Component::from_path(shim).expect("Unable to parse shim");
+    let code = component::Component::from_path(code).expect("Unable to parse code");
 
-        // Determine the memory range for the enclave.
-        let span: Span<_, _> = shim.range().into();
-        let span = Span {
-            start: span.start,
-            count: span.count.next_power_of_two(),
-        };
-
-        // Initiate the enclave building process.
-        let mut builder = builder::Builder::new(span).expect("Unable to create builder");
-
-        // Load the shim segments.
-        for seg in shim.segments.iter() {
-            let mut src = unsafe { seg.src.align_to().1 };
-            let mut off = seg.dst.start - span.start;
-
-            // The first page of the shim entry is the TCS page.
-            if seg.dst.start == shim.entry {
-                builder
-                    .load(&src[..4096], off, SecInfo::tcs())
-                    .expect("Unable to add TCS page");
-                src = &src[4096..];
-                off += 4096;
-            }
-
-            if !src.is_empty() {
-                builder
-                    .load(src, off, SecInfo::reg(seg.rwx))
-                    .expect("Unable to add shim page");
-            }
-        }
-
-        // Load the code segments.
-        for seg in code.segments.iter() {
-            let src = unsafe { seg.src.align_to().1 };
-            let off = seg.dst.start - span.start;
-            builder
-                .load(&src, off, SecInfo::reg(seg.rwx))
-                .expect("Unable to add code page");
-        }
-
-        // Complete the process.
-        (
-            builder.done().expect("Unable to initialize enclave"),
-            code.entry,
-        )
+    // Determine the memory range for the enclave.
+    let span: Span<_, _> = shim.range().into();
+    let span = Span {
+        start: span.start,
+        count: span.count.next_power_of_two(),
     };
+
+    // Initiate the enclave building process.
+    let mut builder = builder::Builder::new(span).expect("Unable to create builder");
+
+    // Load the shim segments.
+    for seg in shim.segments.iter() {
+        let mut src = unsafe { seg.src.align_to().1 };
+        let mut off = seg.dst.start - span.start;
+
+        // The first page of the shim entry is the TCS page.
+        if seg.dst.start == shim.entry {
+            builder
+                .load(&src[..4096], off, SecInfo::tcs())
+                .expect("Unable to add TCS page");
+            src = &src[4096..];
+            off += 4096;
+        }
+
+        if !src.is_empty() {
+            builder
+                .load(src, off, SecInfo::reg(seg.rwx))
+                .expect("Unable to add shim page");
+        }
+    }
+
+    // Load the code segments.
+    for seg in code.segments.iter() {
+        let src = unsafe { seg.src.align_to().1 };
+        let off = seg.dst.start - span.start;
+        builder
+            .load(&src, off, SecInfo::reg(seg.rwx))
+            .expect("Unable to add code page");
+    }
+
+    // Complete the process.
+    (
+        builder.done().expect("Unable to initialize enclave"),
+        code.entry,
+    )
+}
+
+fn main() {
+    let (enclave, entry) = load();
 
     // The main loop event handing is divided into two halves.
     //
