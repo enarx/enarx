3ea3e551ba3b350e9af89d57edd79d949de8e6d3
Nathaniel McCallum <npmccallum@redhat.com>
2020-04-05 20:10:36 -0400

    Dump unsupported syscalls and opcodes
    This lets us immediately see which things need to be supported.

--- enarx-keep-sgx-shim/src/event.rs
+++ enarx-keep-sgx-shim/src/event.rs
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use nolibc::x86_64::error::Number as ErrNo;
 use nolibc::x86_64::syscall::Number as SysCall;
 use sgx_types::ssa::StateSaveArea;
 
-use crate::handler::{Context, Handler};
+use crate::handler::{Context, Handler, Print};
 use crate::Layout;
 
 #[no_mangle]
@@ -17,13 +18,12 @@ pub extern "C" fn event(
     aex: &mut StateSaveArea,
     ctx: &Context,
 ) {
-    match unsafe { core::slice::from_raw_parts(aex.gpr.rip as *const u8, 2) } {
+    let mut h = Handler::new(layout, aex, ctx);
+
+    match unsafe { core::slice::from_raw_parts(h.aex.gpr.rip as *const u8, 2) } {
         // syscall
         [0x0f, 0x05] => {
-            let syscall = aex.gpr.rax.into();
-            let mut h = Handler::new(layout, aex, ctx);
-
-            aex.gpr.rax = match syscall {
+            aex.gpr.rax = match h.aex.gpr.rax.into() {
                 SysCall::READ => h.read(),
                 SysCall::READV => h.readv(),
                 SysCall::WRITE => h.write(),
@@ -34,12 +34,24 @@ pub extern "C" fn event(
                 SysCall::EXIT_GROUP => h.exit_group(None),
                 SysCall::SET_TID_ADDRESS => h.set_tid_address(),
                 SysCall::BRK => h.brk(),
-                _ => h.exit(254),
+
+                syscall => {
+                    h.print("unsupported syscall: ");
+                    h.print(&syscall);
+                    h.print("\n");
+                    ErrNo::ENOSYS.into_syscall()
+                }
             };
 
             aex.gpr.rip += 2;
         }
 
-        _ => Handler::new(layout, aex, ctx).exit(255),
+        r => {
+            let opcode = (r[0] as u16) << 8 | r[1] as u16;
+            h.print("unsupported opcode: ");
+            h.print(&opcode);
+            h.print("\n");
+            h.exit(1)
+        }
     };
 }
