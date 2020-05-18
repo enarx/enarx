48bd2b1525d931735a05000d858410baec775203
Connor Kuehl <ckuehl@redhat.com>
2020-05-18 15:57:07 -0700

    Fix verify() false positive
    OpenSSL's verify() method will only return Err if it experiences some
    catastrophic error that prevents it from making a conclusive decision as
    to whether the public key's signature can be verified to be on the
    digest. It will *NOT* return Err if the key's signature is not on the
    digest; it will return Ok(false).
    
    pub fn verify(&self, signature: &[u8]) -> Result<bool, ErrorStack> {
        unsafe {
            let r =
                EVP_DigestVerifyFinal(self.md_ctx, signature.as_ptr() as *mut _, signature.len());
            match r {
                1 => Ok(true),
                0 => {
                    ErrorStack::get(); // discard error stack
                    Ok(false)
                }
                _ => Err(ErrorStack::get()),
            }
        }
    }
    
    We have simply been assuming that if we get Ok from the verify method
    that the public key's signature has been found on the digest. We must
    inspect the interior `bool` to know whether the signature is verified.
    
    Fixes #520

--- sev/tests/certs.rs
+++ sev/tests/certs.rs
@@ -2,3 +2,14 @@
 
 mod naples;
 mod rome;
+
+use ::sev::certs::*;
+use codicon::Decoder;
+
+#[test]
+fn test_for_verify_false_positive() {
+    // https://github.com/enarx/enarx/issues/520
+    let naples_cek = sev::Certificate::decode(&mut &naples::CEK[..], ()).unwrap();
+    let rome_ask = ca::Certificate::decode(&mut &rome::ASK[..], ()).unwrap();
+    assert!((&rome_ask, &naples_cek).verify().is_err());
+}
--- sev/tests/naples/mod.rs
+++ sev/tests/naples/mod.rs
@@ -12,7 +12,7 @@ const ARK_BAD: &[u8] = include_bytes!("ark.cert.bad");
 const OCA: &[u8] = include_bytes!("oca.cert");
 const ARK: &[u8] = include_bytes!("ark.cert");
 const ASK: &[u8] = include_bytes!("ask.cert");
-const CEK: &[u8] = include_bytes!("cek.cert");
+pub const CEK: &[u8] = include_bytes!("cek.cert");
 const PEK: &[u8] = include_bytes!("pek.cert");
 const PDH: &[u8] = include_bytes!("pdh.cert");
 
