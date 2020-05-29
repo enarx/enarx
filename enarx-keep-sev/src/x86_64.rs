a983c345e5e0d30067b601b68a568d6fbd5e98c4
Connor Kuehl <ckuehl@redhat.com>
2020-05-29 14:53:37 -0700

    Rework const guest addresses to be PhysAddr
    Now that they're PhysAddr right out of the gate, we don't need to error
    check and try to convert these from a primitive. We know they'll work.

--- enarx-keep-sev/src/x86_64.rs
+++ enarx-keep-sev/src/x86_64.rs
@@ -1,23 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use x86_64::structures::paging::page_table::PageTable;
+use x86_64::PhysAddr;
 
 /// The *guest* physical address of the root page table structure.
-pub const PML4_START: u64 = 0x9000;
+pub const PML4_START: PhysAddr = PhysAddr::new_truncate(0x9000);
 
 /// The first page table entry.
-pub const PDPTE_START: u64 = 0xA000;
-
-// The one provided by x86_64 understandably does not impl std::error::Error.
-#[derive(Copy, Clone, Debug)]
-pub struct PhysAddrNotValid(pub u64);
-impl std::error::Error for PhysAddrNotValid {}
-
-impl std::fmt::Display for PhysAddrNotValid {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}({:#x})", "PhysAddrNotValid", self.0)
-    }
-}
+pub const PDPTE_START: PhysAddr = PhysAddr::new_truncate(0xA000);
 
 #[repr(C)]
 pub struct PageTables {
