f1e720b31bd6ae2db1ec6a34586aba6a21831439
Nathaniel McCallum <npmccallum@redhat.com>
2020-08-20 15:57:30 -0400

    Fix EREPORT example
    If the "asm" feature is disabled, we get an error during `cargo test`.

--- sgx/examples/generate_report.rs
+++ sgx/examples/generate_report.rs
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use sgx::attestation_types::ti;
+#[cfg(not(feature = "asm"))]
+fn main() {}
 
 #[cfg(feature = "asm")]
 fn main() {
+    use sgx::attestation_types::ti;
+
     let target_info: ti::TargetInfo = Default::default();
     let data = ti::ReportData([0u8; 64]);
     let report = unsafe { target_info.get_report(&data) };
