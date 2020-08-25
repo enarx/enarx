a85c8075540bb221eb1a1286e77e21820f5cdd96
Connor Kuehl <ckuehl@redhat.com>
2020-08-25 17:34:11 -0500

    Enable SEV protections on VM launch
    Because 'cargo test' is multithreaded by default, the SEV launch tests
    are mutually exclusive with the SEV unit tests that are gated behind
    the "dangerous_tests" feature flag. Manipulating the SEV platform state
    during a launch almost always results in a corrupted launch context.
    
    For example: In the time it takes for Thread A to export the certificate
    chain to prepare for an SEV launch, another thread has already
    manipulated the platform state with a test like platform_reset or
    something similar.

--- sev/tests/launch.rs
+++ sev/tests/launch.rs
@@ -1,14 +1,65 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use sev::certs::Chain;
+use sev::firmware::Firmware;
+use sev::launch::{HeaderFlags, Launcher, Policy};
+use sev::session::Session;
+use sev::Generation;
+
 use kvm_bindings::kvm_userspace_memory_region;
 use kvm_ioctls::{Kvm, VcpuExit};
+
+use std::convert::TryFrom;
+use std::fs::File;
+use std::io::Write;
 
 const CODE: &[u8] = &[
     0xf4, // hlt
 ];
 
+fn get_cert_chain(sev: &mut Firmware) -> Chain {
+    use codicon::Decoder;
+
+    // If you are running this test suite often without manipulating the platform
+    // state (i.e., platform reset) it is recommended that you export the entire
+    // chain to a file and use this environment variable to avoid getting rate-limited
+    // by the AMD web service (which will cause a panic).
+    if let Ok(cached_chain) = std::env::var("CHAIN") {
+        let mut chain = File::open(cached_chain).unwrap();
+
+        Chain::decode(&mut chain, ()).unwrap()
+    } else {
+        let mut platform = sev.pdh_cert_export().unwrap();
+
+        let id = sev.get_identifer().unwrap();
+        let url = format!("https://kdsintf.amd.com/cek/id/{}", id);
+
+        let mut resp = reqwest::blocking::get(&url)
+            .unwrap()
+            .error_for_status()
+            .unwrap();
+
+        let mut cek = vec![];
+        let _ = resp.copy_to(&mut cek).unwrap();
+        platform.cek = sev::certs::sev::Certificate::decode(&mut &cek[..], ()).unwrap();
+
+        let ca = Generation::try_from(&platform).unwrap().into();
+
+        Chain { sev: platform, ca }
+    }
+}
+
+#[cfg_attr(any(not(has_sev), feature = "dangerous_tests"), ignore)]
 #[test]
 fn sev() {
+    let mut sev = Firmware::open().unwrap();
+    let build = sev.platform_status().unwrap().build;
+    let chain = get_cert_chain(&mut sev);
+
+    let policy = Policy::default();
+    let session = Session::try_from(policy).unwrap();
+    let start = session.start(chain).unwrap();
+
     let kvm = Kvm::new().unwrap();
     let vm = kvm.create_vm().unwrap();
 
@@ -42,6 +93,25 @@ fn sev() {
             .write(&zeros[..])
             .unwrap();
     }
+
+    let guest_addr = unsafe { std::slice::from_raw_parts(address_space, MEM_SIZE) };
+
+    let (mut launcher, measurement) = {
+        let launcher = Launcher::new(&vm, &sev).unwrap();
+        let launcher = launcher.start(start).unwrap();
+        let launcher = launcher.measure().unwrap();
+        let measurement = launcher.measurement();
+        (launcher, measurement)
+    };
+
+    let session = session.measure().unwrap();
+    let session = session.verify(build, measurement).unwrap();
+    let secret = session.secret(HeaderFlags::default(), CODE).unwrap();
+
+    launcher.pin_pages(guest_addr).unwrap();
+    launcher.inject(secret, &guest_addr[0]).unwrap();
+
+    let _handle = launcher.finish().unwrap();
 
     let vcpu = vm.create_vcpu(0).unwrap();
     let mut sregs = vcpu.get_sregs().unwrap();
