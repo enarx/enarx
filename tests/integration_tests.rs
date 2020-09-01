0426b9e1cf2ed0614ec9681d39b777a829b32c8c
Nathaniel McCallum <npmccallum@redhat.com>
2020-09-01 17:53:55 -0400

    Add read_item() helper function
    This should clean up future test reads.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::io::Read;
+use std::mem::{size_of, MaybeUninit};
 use std::path::Path;
 use std::process::{Command, Stdio};
+use std::slice::from_raw_parts_mut;
 use std::time::Duration;
+
 use wait_timeout::ChildExt;
 
 const CRATE: &str = env!("CARGO_MANIFEST_DIR");
@@ -42,6 +45,14 @@ fn run_test(bin: &str, status: i32) -> std::process::Child {
     child
 }
 
+fn read_item<T: Copy>(mut rdr: impl Read) -> std::io::Result<T> {
+    let mut item = MaybeUninit::uninit();
+    let ptr = item.as_mut_ptr() as *mut u8;
+    let buf = unsafe { from_raw_parts_mut(ptr, size_of::<T>()) };
+    rdr.read_exact(buf)?;
+    Ok(unsafe { item.assume_init() })
+}
+
 #[test]
 fn exit_zero() {
     run_test("exit_zero", 0);
@@ -59,24 +70,18 @@ fn clock_gettime() {
 
 #[test]
 fn write_stdout() {
-    let mut buf = [0u8; 3];
     let child = run_test("write_stdout", 0);
-    child
-        .stdout
-        .unwrap()
-        .read(&mut buf)
-        .expect("failed to read child stdout");
+    let stdout = child.stdout.unwrap();
+
+    let buf: [u8; 3] = read_item(stdout).unwrap();
     assert_eq!("hi\n", String::from_utf8(buf.to_vec()).unwrap());
 }
 
 #[test]
 fn write_stderr() {
-    let mut buf = [0u8; 3];
     let child = run_test("write_stderr", 0);
-    child
-        .stderr
-        .unwrap()
-        .read(&mut buf)
-        .expect("failed to read child stderr");
+    let stdout = child.stderr.unwrap();
+
+    let buf: [u8; 3] = read_item(stdout).unwrap();
     assert_eq!("hi\n", String::from_utf8(buf.to_vec()).unwrap());
 }
