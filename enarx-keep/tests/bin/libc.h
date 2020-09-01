0322db5d4f7e5b260a265af2bdb20bf52c4430cd
Nathaniel McCallum <npmccallum@redhat.com>
2020-09-01 17:57:53 -0400

    Reimplement write() tests on "libc.h"

--- enarx-keep/tests/bin/libc.h
+++ enarx-keep/tests/bin/libc.h
@@ -1,4 +1,5 @@
 #include <sys/syscall.h>
+#include <unistd.h>
 
 void _exit(int status) {
     asm("syscall; ud2" :: "a" (SYS_exit), "D" (status));
@@ -8,4 +9,16 @@ void _exit(int status) {
 int main(void);
 void _start(void) {
     _exit(main());
+}
+
+ssize_t write(int fd, const void *buf, size_t count) {
+    ssize_t rax;
+
+    asm(
+        "syscall"
+        : "=a" (rax)
+        : "a" (SYS_write), "D" (fd), "S" (buf), "d" (count)
+    );
+
+    return rax;
 }
