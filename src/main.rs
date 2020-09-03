730e98020307d6e4a8865b411ce6234dfcad15b6
Nathaniel McCallum <npmccallum@redhat.com>
2020-09-03 16:18:37 -0400

    Add getting started documentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,50 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! Loads keeps from their backends.
+//! This crate provides the `enarx-keepldr` executable which loads `static-pie`
+//! binaries into an Enarx Keep - that is a hardware isolated environment using
+//! technologies such as Intel SGX or AMD SEV.
+//!
+//! # Install Dependencies
+//!
+//! ## Fedora
+//!
+//!     $ sudo dnf install git curl gcc pkg-config openssl-devel musl-gcc
+//!
+//! ## Debian / Ubuntu
+//!
+//!     $ sudo apt update
+//!     $ sudo apt install git curl gcc pkg-config libssl-dev musl-tools python3-minimal
+//!
+//! # Install Rust, Nightly and the MUSL target
+//!
+//!     $ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+//!     $ source $HOME/.cargo/env
+//!     $ rustup toolchain install nightly --allow-downgrade -t x86_64-unknown-linux-musl
+//!
+//! # Build
+//!
+//!     $ git clone https://github.com/enarx/enarx-keepldr
+//!     $ cd enarx-keepldr/
+//!     $ cargo +nightly build
+//!
+//! # Run Tests
+//!
+//!     $ cargo +nightly test
+//!
+//! # Build and Run an Application
+//!
+//!     $ cat > test.c <<EOF
+//!     #include <stdio.h>
+//!
+//!     int main() {
+//!         printf("Hello World!\n");
+//!         return 0;
+//!     }
+//!     EOF
+//!
+//!     $ musl-gcc -static-pie -fPIC -o test test.c
+//!     $ target/debug/enarx-keepldr exec ./test
+//!     Hello World!
 
 #![deny(clippy::all)]
 #![deny(missing_docs)]
