0344d13d6bcb9fcc3b59004c6b351e19fe8a0ff5
Lily Sturmann <lsturman@redhat.com>
2020-09-03 10:21:46 -0400

    Remove integration-tests crate

--- integration-tests/src/bin/watchdog.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-fn main() {
-    #[allow(clippy::empty_loop)]
-    loop {}
-}
--- integration-tests/src/main.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-#![deny(clippy::all)]
-#![deny(missing_docs)]
-#![allow(missing_docs)]
-
-fn main() {}
