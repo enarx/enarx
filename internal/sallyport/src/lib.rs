9b7352482377bae9d9e2e25c1ca6efb2173709cb
Harald Hoyer <harald@redhat.com>
2020-11-05 14:36:37 +0100

    fix(sallyport): Remove obsolete doc section
    This doc section seems to be a leftover, when some module was called `proxy`.
    
    Signed-off-by: Harald Hoyer <harald@redhat.com>

--- internal/sallyport/src/lib.rs
+++ internal/sallyport/src/lib.rs
@@ -8,9 +8,4 @@
 #![deny(clippy::all)]
 #![cfg_attr(not(test), no_std)]
 
-//! The `proxy` module contains structures used to facilitate communication between
-//! the microkernel and the hypervisor. This is referred to as "proxying" in the
-//! project literature. This is a very thin and low-level layer that is meant to
-//! be as transparent as possible.
-
 include!("../../../src/sallyport/mod.rs");
