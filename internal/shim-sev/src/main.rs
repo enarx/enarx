9b5d82a5963735a31e2bd314615d50083c70a3db
Harald Hoyer <harald@redhat.com>
2020-11-10 17:11:06 +0100

    feat(shim-sev): Provide a stack trace in the panic output
    To ease debugging, get a stack traceback on panic printed to stderr on
    the host along with the panic information.
    
    Signed-off-by: Harald Hoyer <harald@redhat.com>

--- internal/shim-sev/src/main.rs
+++ internal/shim-sev/src/main.rs
@@ -34,15 +34,21 @@ pub mod random;
 pub mod syscall;
 pub mod usermode;
 
-use crate::addr::ShimVirtAddr;
+use crate::addr::{ShimVirtAddr, SHIM_VIRT_OFFSET};
+use crate::frame_allocator::FRAME_ALLOCATOR;
 use crate::hostcall::HOST_CALL;
 use crate::pagetables::switch_sallyport_to_unencrypted;
+use crate::paging::SHIM_PAGETABLE;
+use crate::payload::PAYLOAD_VIRT_ADDR;
 use core::convert::TryFrom;
+use core::mem::size_of;
 use core::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 pub use hostlib::BootInfo;
 use primordial::Address;
 use sallyport::Block;
-use spinning::RwLock;
+use spinning::{OnceState, RwLock};
+use x86_64::structures::paging::MapperAllSizes;
+use x86_64::VirtAddr;
 
 static C_BIT_MASK: AtomicU64 = AtomicU64::new(0);
 
@@ -134,25 +140,94 @@ pub extern "C" fn shim_main() -> ! {
 /// Reverts to a triple fault, which causes a `#VMEXIT` and a KVM shutdown,
 /// if it can't print the panic and exit normally with an error code.
 #[panic_handler]
-#[allow(clippy::empty_loop)]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
     use asm::_enarx_asm_triple_fault;
 
     static mut ALREADY_IN_PANIC: AtomicBool = AtomicBool::new(false);
 
-    unsafe {
-        if ALREADY_IN_PANIC
-            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
-            .is_ok()
-        {
-            // panic info is useful
-            HOST_CALL.force_unlock();
-            eprintln!("{}", info);
-            // FIXME: might want to have a custom panic hostcall
-            hostcall::shim_exit(255);
+    // Don't print anything, if the FRAME_ALLOCATOR is not yet initialized
+    if FRAME_ALLOCATOR.state().eq(&OnceState::Initialized) {
+        unsafe {
+            if ALREADY_IN_PANIC
+                .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+                .is_ok()
+            {
+                // panic info is useful
+                if HOST_CALL.is_locked() {
+                    HOST_CALL.force_unlock();
+                }
+                print::_eprint(format_args!("{}\n", info));
+                stack_trace();
+                // FIXME: might want to have a custom panic hostcall
+                hostcall::shim_exit(255);
+            }
         }
     }
 
     // provoke triple fault, causing a VM shutdown
     unsafe { _enarx_asm_triple_fault() };
+}
+
+#[inline(never)]
+unsafe fn stack_trace() {
+    let mut rbp: usize;
+
+    asm!("mov {}, rbp", out(reg) rbp);
+
+    print::_eprint(format_args!("TRACE:\n"));
+
+    if SHIM_PAGETABLE.is_locked() {
+        SHIM_PAGETABLE.force_unlock_read();
+    }
+
+    if BOOT_INFO.is_locked() {
+        BOOT_INFO.force_unlock_read();
+    }
+
+    let bootinfo = BOOT_INFO.read();
+    let shim_start = bootinfo.unwrap().shim.start;
+    let shim_offset = shim_start.checked_add(SHIM_VIRT_OFFSET as _).unwrap();
+
+    let active_table = SHIM_PAGETABLE.read();
+
+    //Maximum 64 frames
+    for _frame in 0..64 {
+        if let Some(rip_rbp) = rbp.checked_add(size_of::<usize>() as _) {
+            if active_table
+                .translate_addr(VirtAddr::new(rbp as _))
+                .is_some()
+                && active_table
+                    .translate_addr(VirtAddr::new(rip_rbp as _))
+                    .is_some()
+            {
+                let rip = *(rip_rbp as *const usize);
+                if let Some(rip) = rip.checked_sub(1) {
+                    if rip == 0 {
+                        break;
+                    }
+
+                    if let Some(rip) = rip.checked_sub(shim_offset) {
+                        print::_eprint(format_args!("  0x{:>016x}\n", rip));
+                        rbp = *(rbp as *const usize);
+                    } else if PAYLOAD_VIRT_ADDR.state().eq(&OnceState::Initialized) {
+                        if let Some(rip) = rip.checked_sub(PAYLOAD_VIRT_ADDR.read().as_u64() as _) {
+                            print::_eprint(format_args!("P 0x{:>016x}\n", rip));
+                            rbp = *(rbp as *const usize);
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    // RIP zero
+                    break;
+                }
+            } else {
+                // RBP NOT MAPPED
+                break;
+            }
+        } else {
+            // RBP OVERFLOW
+            break;
+        }
+    }
 }
