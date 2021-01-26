95dcb02ae6e49432385a0c999af07480a3b3b3c1
Lily Sturmann <lsturman@redhat.com>
2021-01-25 19:04:16 -0500

    Update expected values in SGX attestation tests
    This allows the req_ti and req_quote unit tests to pass again. They became
    outdated when we moved to the V3 Quote.
    
    Signed-off-by: Lily Sturmann <lsturman@redhat.com>

--- tests/bin/sgx_get_att_quote.c
+++ tests/bin/sgx_get_att_quote.c
@@ -6,12 +6,24 @@
  * Quote from get_attestation() and check that the first bytes of
  * the returned Quote in buf match expected values. */
 
-// TODO: Determine expected Quote value
-
 int main(void) {
     int* nonce = NULL;
     unsigned char buf[4598];
     size_t technology;
+    int i = 0;
+    unsigned char expected[32] = { 
+        3, 0, 2, 0, 0, 0, 0, 0, 5, 0,
+        10, 0, 147, 154, 114, 51, 247, 156, 76, 169,
+        148, 10, 13, 179, 149, 127, 6, 7, 14, 153,
+        112, 145
+    };
+    unsigned char dummy[32] = {
+        44, 44, 44, 44, 44, 44, 44, 44, 44, 44,
+        44, 44, 44, 44, 44, 44, 44, 44, 44, 44,
+        44, 44, 44, 44, 44, 44, 44, 44, 44, 44,
+        44, 44
+    };
+
 
     ssize_t size = get_att(nonce, sizeof(nonce), buf, sizeof(buf), &technology);
 
@@ -21,6 +33,15 @@ int main(void) {
 
     if (size < 0)
         return 1;
+
+    /* check beginning of quote matches expected value */
+    for (i; i < 32; i++)
+    {
+        if (buf[i] != expected[i] && buf[i]!= dummy[i])
+        {
+            return 1;
+        }
+    }
 
     return 0;
 }
