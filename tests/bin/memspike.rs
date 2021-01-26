8109b139abc3b250ae9141a56c266a6a18c2a95f
Harald Hoyer <harald@redhat.com>
2021-01-26 08:57:44 +0100

    feat(clippy): add clippy for the integration tests
    Signed-off-by: Harald Hoyer <harald@redhat.com>

--- tests/bin/memspike.rs
+++ tests/bin/memspike.rs
@@ -2,10 +2,6 @@
 //! VM-based keeps. This will help test the ballooning itself as well
 //! as memory pinning for SEV.
 
-use std::io;
-
-fn main() -> io::Result<()> {
+fn main() {
     let _alloc: Vec<u8> = Vec::with_capacity(40_000_000);
-
-    Ok(())
 }
