948059258094d9973213fa593442add366559e72
Will Woods <will@profian.com>
2021-09-30 20:54:09 -0400

    wasmldr: read module from fd 3 if no path given
    This commit makes wasmldr try to read the wasm module from fd3 if no
    file path was given. (Since keepldr doesn't currently pass arguments to
    wasmldr, this will be the default behavior.)
    
    This means you should be able to run a .wasm module in a keep by doing:
    
        cargo run -- exec 3< hello.wasm
    
    This behavior will likely change soon, but hey.. Hello World!
    
    Signed-off-by: Will Woods <will@profian.com>

--- internal/wasmldr/src/main.rs
+++ internal/wasmldr/src/main.rs
@@ -36,6 +36,7 @@ use structopt::StructOpt;
 
 use std::fs::File;
 use std::io::Read;
+use std::os::unix::io::{FromRawFd, RawFd};
 
 fn main() {
     // Initialize the logger, taking settings from the default env vars
@@ -47,9 +48,13 @@ fn main() {
     let opts = cli::RunOptions::from_args();
     info!("opts: {:#?}", opts);
 
-    info!("reading {:?}", opts.module);
-    // TODO: don't just panic here...
-    let mut reader = File::open(&opts.module).expect("Unable to open file");
+    let mut reader = if let Some(module) = opts.module {
+        info!("reading module from {:?}", &module);
+        File::open(&module).expect("Unable to open file")
+    } else {
+        info!("reading module from fd 3");
+        unsafe { File::from_raw_fd(RawFd::from(3)) }
+    };
 
     let mut bytes = Vec::new();
     reader
