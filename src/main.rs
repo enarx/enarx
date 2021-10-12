26fe7b1fdf0b1850f8640bf8dc52d6b5c9cb57a8
Will Woods <will@profian.com>
2021-10-12 15:16:14 -0400

    Update README.md to match current reality
    Update the doc comment in main.rs to demonstrate using `enarx run` to
    run a WebAssembly module, and the new `--backend` arg (in addition to
    the `ENARX_BACKEND` environment variable).
    
    Signed-off-by: Will Woods <will@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! This crate provides the `enarx` executable which loads `static-pie`
-//! binaries into an Enarx Keep - that is a hardware isolated environment using
+//! This crate provides the `enarx` executable, which is a tool for running
+//! code inside an Enarx Keep - that is a hardware isolated environment using
 //! technologies such as Intel SGX or AMD SEV.
 //!
 //! # Building
@@ -12,36 +12,28 @@
 //!
 //!     $ cargo test
 //!
-//! # Build and Run an Application
+//! # Build and run a WebAssembly module
 //!
-//!     $ cat > test.c <<EOF
-//!     #include <stdio.h>
-//!
-//!     int main() {
-//!         printf("Hello World!\n");
-//!         return 0;
-//!     }
-//!     EOF
-//!
-//!     $ musl-gcc -static-pie -fPIC -o test test.c
-//!     $ target/debug/enarx exec ./test
-//!     Hello World!
+//!     $ cargo init --bin hello-world
+//!     $ cd hello-world
+//!     $ echo 'fn main() { println!("Hello, Enarx!"); }' > src/main.rs
+//!     $ cargo build --release --target=wasm32-wasi
+//!     $ enarx run target/wasm32-wasi/release/hello-world.wasm
+//!     Hello, Enarx!
 //!
 //! # Select a Different Backend
 //!
-//! `enarx exec` will probe the machine it is running on
-//! in an attempt to deduce an appropriate deployment backend unless
-//! that target is already specified in an environment variable
-//! called `ENARX_BACKEND`.
+//! `enarx` will probe the machine it is running on in an attempt to deduce an
+//! appropriate deployment backend. To see what backends are supported on your
+//! system, run:
 //!
-//! To see what backends are supported on your system, run:
+//!     $ enarx info
 //!
-//!     $ target/debug/enarx info
+//! You can manually select a backend with the `--backend` option, or by
+//! setting the `ENARX_BACKEND` environment variable:
 //!
-//! To manually select a backend, set the `ENARX_BACKEND` environment
-//! variable:
-//!
-//!     $ ENARX_BACKEND=sgx target/debug/enarx exec ./test
+//!     $ enarx run --backend=sgx test.wasm
+//!     $ ENARX_BACKEND=sgx enarx run test.wasm
 //!
 //! Note that some backends are conditionally compiled. They can all
 //! be compiled in like so:
