06d63ccae00095509254254b64f0c1cd09434841
Harald Hoyer <harald@profian.com>
2021-10-25 11:17:50 +0200

    feat(shim-sev): use sysretq for initial usermode switch
    - beautify assembler code
    - use `sysretq` to switch to usermode
    - use `xrstor` to clear the xCPU state
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- internal/shim-sev/src/usermode.rs
+++ internal/shim-sev/src/usermode.rs
@@ -2,9 +2,7 @@
 
 //! switch to Ring 3 aka usermode
 
-use crate::gdt::{USER_CODE_SEGMENT, USER_DATA_SEGMENT};
-
-use x86_64::registers::rflags::RFlags;
+use const_default::ConstDefault;
 
 /// Enter Ring 3
 ///
@@ -13,49 +11,52 @@ use x86_64::registers::rflags::RFlags;
 /// Because the caller can give any `entry_point` and `stack_pointer`
 /// including 0, this function is unsafe.
 pub unsafe fn usermode(ip: u64, sp: u64) -> ! {
-    // switch to ring3 with the stack setup
-    // and registers cleared
+    static XSAVE: xsave::XSave = xsave::XSave::DEFAULT;
+
+    // switch to ring3 with the stack setup and registers cleared
     asm!(
-        "push     {USER_DATA_SEGMENT}",
-        "push     {SP}",
-        "push     {RFLAGS}",
-        "push     {USER_CODE_SEGMENT}",
-        "push     {IP}",
-        "xor      rax, rax",
-        "xor      rbx, rbx",
-        "xor      rcx, rcx",
-        "xor      rdx, rdx",
-        "xor      rsi, rsi",
-        "xor      rdi, rdi",
-        "xor      rbp, rbp",
-        "xor      r8,  r8",
-        "xor      r9,  r9",
-        "xor      r10, r10",
-        "xor      r11, r11",
-        "xor      r12, r12",
-        "xor      r13, r13",
-        "xor      r14, r14",
-        "xor      r15, r15",
+        // IP is in rcx, required by sysret
+
+        "pushfq",                   // Copy RFLAGS into r11, required by sysret
+        "pop    r11",
+
+        // Load userspace stack
+        "mov    rsp,    {SP}",
+
+        "mov    rdx,    ~0",        // Set mask for xrstor in rdx
+        "mov    rax,    ~0",        // Set mask for xrstor in rax
+        "xrstor [rip + {XSAVE}]",   // Clear xCPU state with synthetic state
+
+        "xor    rax,    rax",
+        "xor    rbx,    rbx",
+        "xor    rdx,    rdx",
+        "xor    rsi,    rsi",
+        "xor    rdi,    rdi",
+        "xor    rbp,    rbp",
+        "xor    r8,     r8",
+        "xor    r9,     r9",
+        "xor    r10,    r10",
+        "xor    r12,    r12",
+        "xor    r13,    r13",
+        "xor    r14,    r14",
+        "xor    r15,    r15",
 
         // clear all segment selectors
-        "mov      ds,  r11",
-        "mov      es,  r11",
-        "mov      fs,  r11",
-        "mov      gs,  r11",
+        "mov    ds,     r10",
+        "mov    es,     r10",
+        "mov    fs,     r10",
 
-        // clear the FPU
-        "fninit",
+        "swapgs",
+        // swapgs variant of Spectre V1. Disable speculation past this point
+        "lfence",
 
-        // do a simulated return from interrupt
-        // this sets the segments and rip from the stack
-        "iretq",
+        "sysretq",
 
-        USER_DATA_SEGMENT = const USER_DATA_SEGMENT,
+        XSAVE = sym XSAVE,
+
         SP = in(reg) sp,
-        RFLAGS = const RFlags::INTERRUPT_FLAG.bits(),
-        USER_CODE_SEGMENT = const USER_CODE_SEGMENT,
-        IP = in(reg) ip,
+        in("rcx") ip,
 
-        options(noreturn, nomem)
+        options(noreturn)
     )
 }
