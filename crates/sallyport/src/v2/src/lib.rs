9eab9681bbd2e3e47d5a248d4aadf435ab951c0f
Roman Volosatovs <roman@profian.com>
2021-11-09 11:16:36 +0100

    v2: Define `Error` and `Result` types
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/v2/src/lib.rs
+++ crates/sallyport/src/v2/src/lib.rs
@@ -1,1 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
+
+#![cfg_attr(not(test), no_std)]
+
+/// Error type used within this crate.
+pub type Error = libc::c_int;
+
+/// Result type returned by functionality exposed by this crate.
+pub type Result<T> = core::result::Result<T, Error>;
