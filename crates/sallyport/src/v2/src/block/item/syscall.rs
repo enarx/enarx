0cf5fc333b3d7bfa152d695441c58067845592a4
Roman Volosatovs <roman@profian.com>
2021-12-15 18:22:34 +0100

    v2/block/item: Make `syscall::Header::num` `usize`
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/v2/src/block/item/syscall.rs
+++ crates/sallyport/src/v2/src/block/item/syscall.rs
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use libc::c_long;
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(C, align(8))]
 pub struct Header {
     pub item: super::Header,
-    pub num: c_long,
+    pub num: usize,
 }
