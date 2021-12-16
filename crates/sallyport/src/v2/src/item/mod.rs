67d267801a1a293ced30ab986d6fbda1ef0e1708
Roman Volosatovs <roman@profian.com>
2021-12-16 15:49:39 +0100

    v2/item: Implement `TryFrom<usize>` for `Kind`
    Co-authored-by: Harald Hoyer <harald@profian.com>
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/v2/src/item/mod.rs
+++ crates/sallyport/src/v2/src/item/mod.rs
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::Error;
+
+use core::convert::TryFrom;
+use libc::EINVAL;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(usize)]
 pub enum Kind {
     End = 0x00,
 
     Syscall = 0x01,
+}
+
+impl TryFrom<usize> for Kind {
+    type Error = Error;
+
+    #[inline]
+    fn try_from(kind: usize) -> Result<Self, Self::Error> {
+        match kind {
+            kind if kind == Kind::End as _ => Ok(Kind::End),
+            kind if kind == Kind::Syscall as _ => Ok(Kind::Syscall),
+            _ => Err(EINVAL),
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
