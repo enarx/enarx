3d845362e2d254d2da0247cac1d25328298605f0
bstrie <865233+bstrie@users.noreply.github.com>
2022-01-19 09:58:54 +0000

    Add --json flag to `enarx info`
    Closes #1008.
    
    Signed-off-by: bstrie <865233+bstrie@users.noreply.github.com>

--- src/cli/info.rs
+++ src/cli/info.rs
@@ -6,34 +6,44 @@ use std::ops::Deref;
 
 /// Show details about backend support on this system
 #[derive(StructOpt, Debug)]
-pub struct Options {}
+pub struct Options {
+    #[structopt(short, long)]
+    /// Emit JSON rather than human-readable output
+    json: bool,
+}
 
 impl Options {
     /// Display nicely-formatted info about each backend
     pub fn display(self) -> Result<()> {
         use colorful::*;
 
-        for backend in BACKENDS.deref() {
-            println!("Backend: {}", backend.name());
+        let backends = BACKENDS.deref();
 
-            let data = backend.data();
+        if self.json {
+            println!("{}", serde_json::to_string_pretty(&backends)?);
+        } else {
+            for backend in backends {
+                println!("Backend: {}", backend.name());
 
-            for datum in &data {
-                let icon = match datum.pass {
-                    true => "✔".green(),
-                    false => "✗".red(),
-                };
+                let data = backend.data();
 
-                if let Some(info) = datum.info.as_ref() {
-                    println!(" {} {}: {}", icon, datum.name, info);
-                } else {
-                    println!(" {} {}", icon, datum.name);
+                for datum in &data {
+                    let icon = match datum.pass {
+                        true => "✔".green(),
+                        false => "✗".red(),
+                    };
+
+                    if let Some(info) = datum.info.as_ref() {
+                        println!(" {} {}: {}", icon, datum.name, info);
+                    } else {
+                        println!(" {} {}", icon, datum.name);
+                    }
                 }
-            }
 
-            for datum in &data {
-                if let Some(mesg) = datum.mesg.as_ref() {
-                    println!("\n{}\n", mesg);
+                for datum in &data {
+                    if let Some(mesg) = datum.mesg.as_ref() {
+                        println!("\n{}\n", mesg);
+                    }
                 }
             }
         }
