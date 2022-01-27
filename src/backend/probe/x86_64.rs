130f1aebe36c4945dd81ba06c5bc9bb21ac4c1cf
Nicholas Farshidmehr <nick385827184@gmail.com>
2022-01-27 16:06:27 -0500

    Show CPU model in `enarx info`.
    - Use constants for CPU manufacturer strings ("AuthenticAMD", "GenuineIntel").
    - Create a helper function Vendor::id() for getting the CPU manufacturer string.
    - Print the CPU identifier when using the KVM backend.
    - Don't use .unwrap() or .transmute(..) in Vendor::get(..)
    - Share code for retrieving the CPU identifier (manufacturer string or model name) with CpuId::cpu_identifier(..)
    
    CpuId::cpu_identifier(..) will take in a CpuIdResult created from `__cpuid` with leaf (EAX) `0x80000000` (Get Highest Extended Function Implemented)
    and the CPU vendor the backend is designed to use or `None` if it may with with multiple.
    
    Then it will get the manufacturer string to easily determine if the CPU is supported.
    
    If CpuIdResult EAX is less than 0x80000004 then the processor brand string is unavailable, which should be rare with real hardware.
    If this happens then [unknown model] will be returned along with the manufacturer string.
    
    Then over a series of `__cpuid` calls with EAX values of `0x80000002`, `0x80000003`, and `0x80000004` the processor brand
    string (model name) is retrieved from the EAX, EBX, ECX, and EDX registers (16 bytes at a time).
    
    For example:
    
    0x80000002:
    
    EAX = "Inte"
    EBX = "l(R)"
    ECX = " Cor"
    EDX = "e(TM"
    
    0x80000003:
    
    EAX = ") i5"
    EBX = "-469"
    ECX = "0K C"
    EDX = "PU @"
    
    0x80000004:
    
    EAX = " 3.5"
    EBX = "0GHz"
    ECX = (all zeroes)
    EDX = (all zeroes)
    
    Each register will contain a small portion of the string, by transmuting it to a byte array we can copy
    each of the 3 the portions of the string at once.
    
    The final C string will be a max of 48 bytes including a null terminator. It is then parsed as UTF8 and
    null characters are removed. It is then returned with the manufacturer string.
    
    A crate `static_assertions` was also added to verify the string length which was derived from some constants in the function.
    
    See the following for additional info:
    
    - https://en.wikipedia.org/wiki/CPUID#EAX=80000002h,80000003h,80000004h:_Processor_Brand_String
    - https://doc.rust-lang.org/core/arch/x86_64/fn.__cpuid.html
    - https://doc.rust-lang.org/core/arch/x86/struct.CpuidResult.html

--- src/backend/probe/x86_64.rs
+++ src/backend/probe/x86_64.rs
@@ -2,10 +2,16 @@
 
 use crate::backend::Datum;
 
-use std::arch::x86_64::{CpuidResult, __cpuid_count};
+use static_assertions::const_assert_eq;
+
+use std::arch::x86_64::{CpuidResult, __cpuid, __cpuid_count};
 use std::convert::From;
 use std::io::{Error, ErrorKind, Result};
-use std::mem::transmute;
+use std::mem::size_of;
+use std::str::from_utf8;
+
+const AUTHENTIC_AMD: &str = "AuthenticAMD";
+const GENUINE_INTEL: &str = "GenuineIntel";
 
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum Vendor {
@@ -16,19 +22,35 @@ pub enum Vendor {
 impl Vendor {
     pub fn get() -> Result<Self> {
         let res = unsafe { __cpuid_count(0x00000000, 0x00000000) };
-        let name: [u8; 12] = unsafe { transmute([res.ebx, res.edx, res.ecx]) };
-        let name = std::str::from_utf8(&name[..]).unwrap();
+        let name = [
+            res.ebx.to_le_bytes(),
+            res.edx.to_le_bytes(),
+            res.ecx.to_le_bytes(),
+        ]
+        .concat();
 
-        Ok(match name {
-            "AuthenticAMD" => Self::Amd,
-            "GenuineIntel" => Self::Intel,
-            _ => {
-                return Err(Error::new(
-                    ErrorKind::Other,
-                    format!("unsupported vendor: {}", name),
-                ))
-            }
-        })
+        let name = from_utf8(&name[..]).map_err(|e| {
+            Error::new(
+                ErrorKind::Other,
+                format!("vendor string parse error: {:#?}", e),
+            )
+        })?;
+
+        match name {
+            AUTHENTIC_AMD => Ok(Self::Amd),
+            GENUINE_INTEL => Ok(Self::Intel),
+            name => Err(Error::new(
+                ErrorKind::Other,
+                format!("unsupported vendor: '{}'", name),
+            )),
+        }
+    }
+
+    pub const fn id(&self) -> &str {
+        match self {
+            Vendor::Amd => AUTHENTIC_AMD,
+            Vendor::Intel => GENUINE_INTEL,
+        }
     }
 }
 
@@ -38,6 +60,62 @@ pub struct CpuId {
     pub subl: u32,
     pub func: fn(CpuidResult) -> (bool, Option<String>),
     pub vend: Option<Vendor>,
+}
+
+impl CpuId {
+    /// Get the Processor Brand String or the CPU manufacturer if unavailable.
+    pub fn cpu_identifier(
+        res: CpuidResult,
+        required_vendor: Option<Vendor>,
+    ) -> (bool, Option<String>) {
+        let vendor = Vendor::get();
+        let supported_cpu =
+            required_vendor.is_none() || required_vendor.as_ref() == vendor.as_ref().ok();
+        let identifier_fmt = |name: Option<&str>| {
+            format!(
+                "{} | {}",
+                name.unwrap_or("[unknown model]"),
+                vendor
+                    .map(|v| v.id().to_string())
+                    .unwrap_or_else(|e| format!("[{}]", e))
+            )
+        };
+
+        if res.eax < 0x80000004 {
+            // The processor brand string (model) is unavailable.
+            return (supported_cpu, Some(identifier_fmt(None)));
+        }
+
+        // https://en.wikipedia.org/wiki/CPUID#EAX=80000002h,80000003h,80000004h:_Processor_Brand_String
+        const LEAVES: [u32; 3] = [0x80000002, 0x80000003, 0x80000004];
+        const CHUNK_SIZE: usize = size_of::<u32>() * 4;
+        const MODEL_LEN: usize = LEAVES.len() * CHUNK_SIZE;
+        const_assert_eq!(MODEL_LEN, 48);
+        let mut model: [u8; MODEL_LEN] = [0; MODEL_LEN];
+
+        // Copy the string in 3 parts over several calls.
+        for (i, leaf) in LEAVES.into_iter().enumerate() {
+            // Get the next part of the model name.
+            let res = unsafe { __cpuid(leaf) };
+            let string_part = [
+                res.eax.to_le_bytes(),
+                res.ebx.to_le_bytes(),
+                res.ecx.to_le_bytes(),
+                res.edx.to_le_bytes(),
+            ]
+            .concat();
+            // Copy all 4 registers into the model name.
+            let start = i * CHUNK_SIZE;
+            let destination = &mut model[start..start + CHUNK_SIZE];
+            destination.clone_from_slice(&string_part);
+        }
+
+        let model_name = String::from_utf8_lossy(model.as_slice());
+        (
+            supported_cpu,
+            Some(identifier_fmt(Some(model_name.trim_matches(char::from(0))))),
+        )
+    }
 }
 
 impl From<&CpuId> for Datum {
