505e65437ad2fc218da87e6c88129f070ba36e60
Harald Hoyer <harald@profian.com>
2022-02-03 16:45:53 +0100

    feat: add a mio tcp echo server example
    ```
    $ cargo build --release
    $ CARGO_TARGET_WASM32_WASI_RUNNER=$(pwd)/helper/test-enarx-wasm.sh cargo run --target wasm32-wasi -p tcp_server
        Finished dev [unoptimized + debuginfo] target(s) in 0.02s
         Running `/home/harald/git/enarx/enarx/helper/test-enarx-wasm.sh target/wasm32-wasi/debug/tcp_server.wasm`
    [WARN  wasmldr] 🌭DEV-ONLY BUILD, NOT FOR PRODUCTION USE🌭
    [DEBUG wasmldr] parsing argv
    [INFO  wasmldr] opts: RunOptions {
            module: None,
            config: None,
        }
    [INFO  wasmldr] reading module from fd 3
    [INFO  wasmldr] reading config from fd 4
    [INFO  wasmldr] running workload
    [DEBUG wasmldr::workload] configuring wasmtime engine
    [DEBUG wasmldr::workload] instantiating wasmtime linker
    [DEBUG wasmldr::workload] adding WASI to linker
    [DEBUG wasmldr::workload] creating WASI context
    [DEBUG wasmldr::workload] Processing loader config Config {
            files: Some(
                [
                    File {
                        type_: "stdio",
                        name: "stdin",
                        addr: None,
                        port: None,
                    },
                    File {
                        type_: "stdio",
                        name: "stdout",
                        addr: None,
                        port: None,
                    },
                    File {
                        type_: "stdio",
                        name: "stderr",
                        addr: None,
                        port: None,
                    },
                    File {
                        type_: "tcp_listen",
                        name: "TEST_TCP_LISTEN",
                        addr: None,
                        port: Some(
                            9000,
                        ),
                    },
                ],
            ),
        }
    [DEBUG wasmldr::workload] creating wasmtime Store
    [DEBUG wasmldr::workload] instantiating module from bytes
    [DEBUG wasmldr::workload] adding module to store
    [DEBUG wasmldr::workload] getting module's default function
    [DEBUG wasmldr::workload] calling function
    Using preopened socket FD 3
    You can connect to the server using `nc`:
     $ nc <IP> <PORT>
    You'll see our welcome message and anything you type will be printed here.
    
    ```
    
    Then from another shell:
    ```
    $ echo ECHO | nc 127.0.0.1 9000
    Hello world!
    ECHO
    ```
    
    and see the output of the Enarx keep:
    ```
    Accepted connection from: 0.0.0.0:0
    Received data: ECHO
    Connection closed
    ```
    
    To modify the port and listen address see the `Enarx.toml` file in the
    example directory.
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- helper/test-enarx-wasm.sh
+++ helper/test-enarx-wasm.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! [ -f "$ENARX_BIN" ]; then
-  (cd ..; cargo build -q)
-  ENARX_BIN=$(dirname $0)/../target/debug/enarx
+if ! command -v "${ENARX_BIN[0]}" &> /dev/null; then
+  if [ -x $(dirname $0)/../target/release/enarx ]; then
+    ENARX_BIN=$(dirname $0)/../target/release/enarx
+  elif [ -x $(dirname $0)/../target/debug/enarx ]; then
+    ENARX_BIN=$(dirname $0)/../target/debug/enarx
+  else
+    (cd $(dirname $0); cd ..; cargo build --release)
+    ENARX_BIN=$(dirname $0)/../target/release/enarx
+  fi
 fi
 
-"$ENARX_BIN" run "$@"
+if [ -f "$CARGO_MANIFEST_DIR"/Enarx.toml ]; then
+  CONFIG="$CARGO_MANIFEST_DIR"/Enarx.toml
+fi
+
+"${ENARX_BIN[@]}" run ${CONFIG+--wasmcfgfile $CONFIG} "$@"
