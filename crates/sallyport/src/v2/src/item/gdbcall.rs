0c2f918a45b076fd77b666461c2346b9b48fa76f
Roman Volosatovs <roman@profian.com>
2022-02-03 16:38:57 +0100

    v2/item/gdbcall: Reorganize
    Define constants/enums after the type and trait definitions for clarity.
    
    Also fixed incorrect ordering of enums - 0x01 comes after 0x00, not 0x02
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/v2/src/item/gdbcall.rs
+++ crates/sallyport/src/v2/src/item/gdbcall.rs
@@ -2,43 +2,6 @@
 
 use core::mem::size_of;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-#[repr(usize)]
-/// Number of an [`Item`](super::Item) of [`Kind::Gdbcall`](super::Kind::Gdbcall).
-pub enum Number {
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::Connection::write]"
-    )]
-    Write = 0x00,
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::Connection::write_all]"
-    )]
-    WriteAll = 0x02,
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::Connection::flush]"
-    )]
-    Flush = 0x03,
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::Connection::on_session_start]"
-    )]
-    OnSessionStart = 0x04,
-
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::ConnectionExt::read]"
-    )]
-    Read = 0x05,
-    #[cfg_attr(
-        feature = "doc",
-        doc = "Call number coresponding to [gdbstub::conn::ConnectionExt::peek]"
-    )]
-    Peek = 0x06,
-}
-
 /// Payload of an [`Item`](super::Item) of [`Kind::Gdbcall`](super::Kind::Gdbcall).
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(C, align(8))]
@@ -56,4 +19,41 @@ impl From<&mut [usize; USIZE_COUNT]> for &mut Payload {
         debug_assert_eq!(size_of::<Payload>(), USIZE_COUNT * size_of::<usize>());
         unsafe { &mut *(buf as *mut _ as *mut _) }
     }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(usize)]
+/// Number of an [`Item`](super::Item) of [`Kind::Gdbcall`](super::Kind::Gdbcall).
+pub enum Number {
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::Connection::write]"
+    )]
+    Write = 0x00,
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::Connection::write_all]"
+    )]
+    WriteAll = 0x01,
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::Connection::flush]"
+    )]
+    Flush = 0x02,
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::Connection::on_session_start]"
+    )]
+    OnSessionStart = 0x03,
+
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::ConnectionExt::read]"
+    )]
+    Read = 0x04,
+    #[cfg_attr(
+        feature = "doc",
+        doc = "Call number coresponding to [gdbstub::conn::ConnectionExt::peek]"
+    )]
+    Peek = 0x05,
 }
