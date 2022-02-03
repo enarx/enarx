dc4e32793f1459efa2cfb67c5103edfd664bb2e5
Roman Volosatovs <roman@profian.com>
2022-02-03 16:12:19 +0100

    v2: Include constants from 0.1 version of the crate
    Removed `MAX_UDP_PACKET_SIZE` constant along the way, since it is not
    used by enarx/enarx
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/v2/src/lib.rs
+++ crates/sallyport/src/v2/src/lib.rs
@@ -58,6 +58,7 @@
 #![feature(slice_ptr_len)]
 #![cfg_attr(not(test), no_std)]
 
+pub mod elf;
 pub mod guest;
 pub mod host;
 pub mod item;
@@ -71,3 +72,30 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 /// Internal representation of a null pointer or [`Option::None`] value in the sallyport block.
 pub const NULL: usize = usize::MAX;
+
+/// The sallyport version
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// The sallyport version requires
+///
+/// This value provides a semver version requirement. It insists that the
+/// other side must use a compatible release to this one. For example, if
+/// the `VERSION` of sallyport is 1.2.3, `REQUIRES` will contain `^1.2.3`.
+///
+/// See [this link](https://docs.rs/semver/1.0.0/semver/enum.Op.html#opcaretcompatible-updates)
+/// for more details.
+pub const REQUIRES: [u8; VERSION.len() + 1] = {
+    let mut value = [0u8; VERSION.len() + 1];
+    let mut i = 0;
+
+    value[0] = b'^';
+    while i < VERSION.len() {
+        value[i + 1] = VERSION.as_bytes()[i];
+        i += 1;
+    }
+
+    value
+};
+
+/// I/O port used to trigger an exit to the host (`#VMEXIT`) for KVM driven shims.
+pub const KVM_SYSCALL_TRIGGER_PORT: u16 = 0xFF;
