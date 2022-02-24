52d14a53940ffb540f1e1f5a5743d9b766760838
Roman Volosatovs <roman@profian.com>
2022-02-24 17:11:55 +0100

    refactor: move arch-specific constants in submodules
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/item/enarxcall/mod.rs
+++ crates/sallyport/src/item/enarxcall/mod.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 pub mod testaso;
 
+pub mod sev;
 pub mod sgx;
 
 use core::mem::size_of;
@@ -38,37 +39,6 @@ impl From<&mut [usize; USIZE_COUNT]> for &mut Payload {
 pub enum Number {
 }
 
-/// `get_attestation` technology return value
-///
-/// See <https://github.com/enarx/enarx-keepldr/issues/31>
-#[allow(dead_code)]
-pub const SEV_TECH: usize = 1;
-
-/// `get_attestation` technology return value
-///
-/// See <https://github.com/enarx/enarx-keepldr/issues/31>
-#[allow(dead_code)]
-pub const SGX_TECH: usize = 2;
-
-/// Size in bytes of expected SGX Quote
-// TODO: Determine length of Quote of PCK cert type
-#[allow(dead_code)]
-pub const SGX_QUOTE_SIZE: usize = 4598;
-
-/// Size in bytes of expected SGX QE TargetInfo
-#[allow(dead_code)]
-pub const SGX_TI_SIZE: usize = 512;
-
-/// Dummy value returned when daemon to return SGX TargetInfo is
-/// not available on the system.
-#[allow(dead_code)]
-pub const SGX_DUMMY_TI: [u8; SGX_TI_SIZE] = [32u8; SGX_TI_SIZE];
-
-/// Dummy value returned when daemon to return SGX Quote is not
-/// available on the system.
-#[allow(dead_code)]
-pub const SGX_DUMMY_QUOTE: [u8; SGX_QUOTE_SIZE] = [44u8; SGX_QUOTE_SIZE];
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -76,5 +46,10 @@ mod tests {
     #[test]
     fn payload_size() {
         assert_eq!(size_of::<Payload>(), USIZE_COUNT * size_of::<usize>())
+    }
+
+    #[test]
+    fn tech_assignments() {
+        assert_ne!(sev::TECH, sgx::TECH);
     }
 }
---
+++ crates/sallyport/src/item/enarxcall/sev.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/// `get_attestation` technology return value
+///
+/// See <https://github.com/enarx/enarx-keepldr/issues/31>
+pub const TECH: usize = 1;
