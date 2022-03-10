24cb5c00a76e93c74ea0edccb3803c9f61021183
Nathaniel McCallum <nathaniel@profian.com>
2022-03-10 08:47:27 +0000

    feat(wasmldr): major rework with new features
    1. Config file parsing is now more rigorous.
    2. We have support for `null` files.
    3. Support for both client and server TLS.
    4. Steward integration.
    
    The main setup flow is now structured as a state machine that steps
    through each of the stages.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

---
+++ internal/wasmldr/src/loader/mod.rs
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: Apache-2.0
+
+mod acquired;
+mod attested;
+mod compiled;
+mod configured;
+mod connected;
+mod requested;
+
+use std::sync::Arc;
+
+use rustls::{ClientConfig, ServerConfig};
+use wasi_common::WasiCtx;
+use wasmtime::{Linker, Store, Val};
+use zeroize::Zeroizing;
+
+use crate::config::Config;
+
+pub struct Configured {
+    config: Config,
+}
+
+pub struct Requested {
+    config: Config,
+    prvkey: Zeroizing<Vec<u8>>,
+    crtreq: Vec<u8>,
+}
+
+pub struct Attested {
+    config: Config,
+    srvcfg: Arc<ServerConfig>,
+    cltcfg: Arc<ClientConfig>,
+}
+
+pub struct Acquired {
+    config: Config,
+    srvcfg: Arc<ServerConfig>,
+    cltcfg: Arc<ClientConfig>,
+
+    webasm: Vec<u8>,
+}
+
+pub struct Compiled {
+    config: Config,
+    srvcfg: Arc<ServerConfig>,
+    cltcfg: Arc<ClientConfig>,
+
+    wstore: Store<WasiCtx>,
+    linker: Linker<WasiCtx>,
+}
+
+pub struct Connected {
+    wstore: Store<WasiCtx>,
+    linker: Linker<WasiCtx>,
+}
+
+pub struct Completed {
+    values: Vec<Val>,
+}
+
+pub struct Loader<T>(T);
+
+impl Loader<Acquired> {
+    #[cfg(test)]
+    pub fn run(module: &[u8]) -> anyhow::Result<Vec<Val>> {
+        use rustls::{server::ResolvesServerCert, RootCertStore};
+
+        struct Resolver;
+
+        impl ResolvesServerCert for Resolver {
+            fn resolve(
+                &self,
+                _client_hello: rustls::server::ClientHello<'_>,
+            ) -> Option<Arc<rustls::sign::CertifiedKey>> {
+                None
+            }
+        }
+
+        let srvcfg = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_cert_resolver(Arc::new(Resolver));
+
+        let cltcfg = ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(RootCertStore::empty())
+            .with_no_client_auth();
+
+        let acquired = Self(Acquired {
+            config: Config::default(),
+            srvcfg: Arc::new(srvcfg),
+            cltcfg: Arc::new(cltcfg),
+            webasm: module.to_vec(),
+        });
+
+        let compiled = acquired.next()?;
+        let connected = compiled.next()?;
+        let completed = connected.next()?;
+        Ok(completed.into())
+    }
+}
+
+impl From<Config> for Loader<Configured> {
+    fn from(config: Config) -> Self {
+        Self(Configured { config })
+    }
+}
+
+impl From<Loader<Completed>> for Vec<Val> {
+    fn from(value: Loader<Completed>) -> Self {
+        value.0.values
+    }
+}
---
+++ internal/wasmldr/src/loader/requested.rs
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use super::{Attested, Loader, Requested};
+
+use std::{io::Read, ops::Deref, sync::Arc};
+
+use anyhow::Result;
+use rustls::{cipher_suite::*, kx_group::*, version::TLS13, *};
+use x509::der::{Decodable, Encodable};
+use x509::PkiPath;
+
+impl Loader<Requested> {
+    // TODO: read this value from the config.
+    const URL: &'static str = "https://steward-dev.onrender.com";
+
+    pub fn next(self) -> Result<Loader<Attested>> {
+        // Send the attestation to the steward.
+        let response = ureq::post(Self::URL)
+            .set("Content-Type", "application/pkcs10")
+            .send_bytes(&self.0.crtreq)?;
+
+        // Read the result.
+        let mut body = Vec::new();
+        response.into_reader().read_to_end(&mut body)?;
+
+        // Decode the certificate chain.
+        let path = PkiPath::from_der(&body)?;
+        let certs = path
+            .0
+            .iter()
+            .rev()
+            .map(|c| Certificate(c.to_vec().unwrap()))
+            .collect::<Vec<_>>();
+
+        // TODO: load this policy from `Config`.
+        let protocol_versions = &[&TLS13];
+        let kx_groups = &[&X25519, &SECP384R1, &SECP256R1];
+        let cipher_suites = &[
+            TLS13_AES_256_GCM_SHA384,
+            TLS13_AES_128_GCM_SHA256,
+            TLS13_CHACHA20_POLY1305_SHA256,
+        ];
+
+        // Set up the server config.
+        let srvcfg = ServerConfig::builder()
+            .with_cipher_suites(cipher_suites)
+            .with_kx_groups(kx_groups)
+            .with_protocol_versions(protocol_versions)?
+            .with_no_client_auth() // TODO: Enable client auth
+            .with_single_cert(certs.clone(), PrivateKey(self.0.prvkey.deref().clone()))?;
+
+        // Set up root store.
+        let mut root_store = RootCertStore::empty();
+        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+            OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        }));
+
+        // Set up client config.
+        let cltcfg = ClientConfig::builder()
+            .with_cipher_suites(cipher_suites)
+            .with_kx_groups(kx_groups)
+            .with_protocol_versions(protocol_versions)?
+            .with_root_certificates(root_store)
+            .with_single_cert(certs, PrivateKey(self.0.prvkey.deref().clone()))?;
+
+        Ok(Loader(Attested {
+            config: self.0.config,
+            srvcfg: Arc::new(srvcfg),
+            cltcfg: Arc::new(cltcfg),
+        }))
+    }
+}
