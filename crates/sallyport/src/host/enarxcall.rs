4b3c490394f8235ebe7a6cd0937ea3cbc38f5aec
Roman Volosatovs <roman@profian.com>
2022-03-11 13:16:47 +0100

    refactor(host): cleanup CPUID match statement
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- crates/sallyport/src/host/enarxcall.rs
+++ crates/sallyport/src/host/enarxcall.rs
@@ -8,12 +8,13 @@ use core::arch::asm;
 use core::arch::x86_64::CpuidResult;
 
 pub(super) unsafe fn execute(call: &mut item::Enarxcall, data: &mut [u8]) -> Result<()> {
+    #[allow(clippy::single_match)]
     match call {
         item::Enarxcall {
-            num,
+            num: Number::Cpuid,
             argv: [leaf, sub_leaf, result_offset, ..],
             ret,
-        } if *num == Number::Cpuid as _ => {
+        } => {
             let result = deref_aligned::<CpuidResult>(data, *result_offset, 1)?;
 
             // Adapted from https://github.com/rust-lang/stdarch/blob/b4a0e07552cf90ef8f1a5b775bf70e4db94b3d63/crates/core_arch/src/x86/cpuid.rs#L51-L89
