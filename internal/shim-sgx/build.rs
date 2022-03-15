279814d23c30e678d3d4ca6b3428d173e993971f
bstrie <865233+bstrie@users.noreply.github.com>
2022-03-15 09:25:22 -0400

    refactor(shims): port to x86_64-unknown-none
    Co-authored-by: Tom Dohrmann <erbse.13@gmx.de>
    Co-authored-by: Harald Hoyer <harald@profian.com>
    Signed-off-by: bstrie <865233+bstrie@users.noreply.github.com>

--- internal/shim-sgx/build.rs
+++ internal/shim-sgx/build.rs
@@ -2,4 +2,7 @@
 
 fn main() {
     println!("cargo:rerun-if-changed=layout.ld");
+    println!("cargo:rustc-link-arg-bin=shim-sgx=-Tlayout.ld");
+    println!("cargo:rustc-link-arg-bin=shim-sgx=-Wl,--sort-section=alignment");
+    println!("cargo:rustc-link-arg-bin=shim-sgx=-nostartfiles");
 }
