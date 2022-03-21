5f24fa56a0fc89d98d9e6702da0c9f3c9f35ee5e
Harald Hoyer <harald@profian.com>
2022-03-21 12:52:06 +0100

    fix(shim-kvm): move linker args to build.rs
    and remove the need for the gcc linker.
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- internal/shim-kvm/build.rs
+++ internal/shim-kvm/build.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
+    println!("cargo:rustc-link-arg-bin=shim-kvm=-Tlayout.ld");
+    println!("cargo:rustc-link-arg-bin=shim-kvm=--sort-section=alignment");
     println!("cargo:rerun-if-changed=layout.ld");
 }
