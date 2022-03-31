cfaf17bb26accd86bd4cb8e666e140a7ca67c055
Paul Pietkiewicz <paul@profian.com>
2022-03-31 06:52:52 -0400

    chore(actions): no longer need to update toolchain in workflows
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- helper/bump-toolchain.sh
+++ helper/bump-toolchain.sh
@@ -6,4 +6,3 @@ echo "Usage: $0 [-h [<TOOLCHAIN_VERSION>]"
 
 echo "Update to rust toolchain version ${TOOLCHAIN_VERSION}"
 sed -i "s/channel = .*/channel = \"${TOOLCHAIN_VERSION}\"/" rust-toolchain.toml
-sed -i "s/toolchain: .*/toolchain: ${TOOLCHAIN_VERSION}/" .github/workflows/{lint,test}.yml
