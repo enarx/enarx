f59b20185a0562f6fa80cd84a2652ed1142446ca
Paul Pietkiewicz <paul@profian.com>
2022-04-11 05:58:55 -0400

    refactor(workflow): fix update workflows to work with bindeps
    Signed-off-by: Paul Pietkiewicz <paul@profian.com>

--- helper/bump-toolchain.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-TOOLCHAIN_VERSION="${1:-nightly-$(date +%Y-%m-%d)}"
-
-echo "Usage: $0 [-h [<TOOLCHAIN_VERSION>]"
-
-echo "Update to rust toolchain version ${TOOLCHAIN_VERSION}"
-sed -i "s/channel = .*/channel = \"${TOOLCHAIN_VERSION}\"/" rust-toolchain.toml
