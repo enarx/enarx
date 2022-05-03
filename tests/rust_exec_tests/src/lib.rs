ffba293cff2e8b0bf8aca78da5655623e8e3cc20
Harald Hoyer <harald@profian.com>
2022-05-03 11:04:33 +0200

    fix: use `core::ffi` for `std::ffi::c_*`
    For `nightly-2022-05-03`:
    
    ```
     19 | use std::ffi::{c_char, c_int, CString};
       |                ^^^^^^  ^^^^^ no `c_int` in `ffi`
       |                |
       |                no `c_char` in `ffi`
    ```
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- tests/rust_exec_tests/src/lib.rs
+++ tests/rust_exec_tests/src/lib.rs
@@ -10,16 +10,16 @@ macro_rules! musl_fsbase_fix {
         /// Overwrite the only location in musl, which uses the `arch_prctl` syscall
         #[no_mangle]
         #[inline(never)]
-        pub extern "C" fn __set_thread_area(p: *mut std::ffi::c_void) -> std::ffi::c_int {
+        pub extern "C" fn __set_thread_area(p: *mut core::ffi::c_void) -> core::ffi::c_int {
             let mut rax: usize = 0;
             if unsafe { core::arch::x86_64::__cpuid(7).ebx } & 1 == 1 {
                 unsafe {
-                    std::arch::asm!("wrfsbase {}", in(reg) p);
+                    core::arch::asm!("wrfsbase {}", in(reg) p);
                 }
             } else {
-                const ARCH_SET_FS: std::ffi::c_int = 0x1002;
+                const ARCH_SET_FS: core::ffi::c_int = 0x1002;
                 unsafe {
-                    std::arch::asm!(
+                    core::arch::asm!(
                     "syscall",
                     inlateout("rax")  libc::SYS_arch_prctl => rax,
                     in("rdi") ARCH_SET_FS,
