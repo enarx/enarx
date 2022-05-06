4d72235705ff90e82bc72f18a9b1dbd46910f408
Harald Hoyer <harald@profian.com>
2022-05-06 13:13:25 +0200

    feat(tests): put tests in submodule
    Prefixes the `cargo test` output and lets you select the tests via
    wildcard.
    
    ```
         Running tests/enarx_exec_tests.rs (target/debug/deps/enarx_exec_tests-63930017383b8aed)
    
    running 6 tests
    test enarx_exec::rust_sev_attestation ... ignored
    test enarx_exec::cpuid ... ok
    test enarx_exec::memory_stress_test ... ok
    test enarx_exec::echo ... ok
    test enarx_exec::memspike ... ok
    test enarx_exec::unix_echo ... ok
    
    test result: ok. 5 passed; 0 failed; 1 ignored; 0 measured; 0 filtered out; finished in 2.09s
    
         Running tests/enarx_syscall_tests.rs (target/debug/deps/enarx_syscall_tests-db31f97089025bc8)
    
    running 20 tests
    test enarx_syscall::get_att ... ignored
    test enarx_syscall::bind ... ok
    test enarx_syscall::close ... ok
    Error: 1
    test enarx_syscall::exit_one ... ok
    test enarx_syscall::clock_gettime ... ok
    test enarx_syscall::exit_zero ... ok
    test enarx_syscall::geteuid ... ok
    test enarx_syscall::sgx_get_att_quote ... ignored
    test enarx_syscall::getegid ... ok
    test enarx_syscall::getgid ... ok
    test enarx_syscall::getuid ... ok
    test enarx_syscall::listen ... ok
    test enarx_syscall::read ... ok
    test enarx_syscall::read_udp ... ok
    test enarx_syscall::readv ... ok
    test enarx_syscall::socket ... ok
    test enarx_syscall::uname ... ok
    test enarx_syscall::write_emsgsize ... ok
    test enarx_syscall::write_stderr ... ok
    test enarx_syscall::write_stdout ... ok
    
    test result: ok. 18 passed; 0 failed; 2 ignored; 0 measured; 0 filtered out; finished in 0.38s
    
         Running tests/enarx_wasm_tests.rs (target/debug/deps/enarx_wasm_tests-cccda760aa27b322)
    
    running 9 tests
    test enarx_wasm::echo ... ok
    test enarx_wasm::check_tcp ... ok
    test enarx_wasm::hello_wasi_snapshot1 ... ok
    test enarx_wasm::return_1 ... ok
    test enarx_wasm::memory_stress_test ... ok
    test enarx_wasm::memspike ... ok
    test enarx_wasm::wasi_snapshot1 ... ok
    Error: default export in '' is not a function
    test enarx_wasm::no_export ... ok
    test enarx_wasm::zerooneone ... ok
    
    test result: ok. 9 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 21.90s
    
    […]
    ```
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

---
+++ tests/enarx_exec/mod.rs
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use std::fs;
+use std::io::{Read, Write};
+use std::os::unix::ffi::OsStrExt;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+use std::time::Duration;
+
+use serial_test::serial;
+use std::sync::Arc;
+use tempfile::Builder;
+
+use crate::common::{assert_eq_slices, run_test};
+
+#[test]
+#[serial]
+fn echo() {
+    let mut input: Vec<u8> = Vec::with_capacity(2 * 1024 * 1024);
+
+    for i in 0..input.capacity() {
+        input.push(i as _);
+    }
+    let input = input.as_slice();
+
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_echo");
+    run_test(bin, 0, input, input, None);
+}
+
+#[test]
+#[serial]
+fn unix_echo() {
+    let tmpdir = Arc::new(Builder::new().prefix("unix_echo").tempdir().unwrap());
+    const FILENAME_IN: &str = "enarx_unix_echo_to_bin";
+    const FILENAME_OUT: &str = "enarx_unix_echo_from_bin";
+    let mut input: Vec<u8> = Vec::with_capacity(2 * 1024 * 1024);
+
+    let _ = fs::remove_file(FILENAME_IN);
+
+    for i in 0..input.capacity() {
+        input.push(i as _);
+    }
+
+    let handle = thread::spawn({
+        let tmpdir = tmpdir.clone();
+        move || {
+            let socket_path = tmpdir.path().join(FILENAME_IN);
+            let mut cnt = 0;
+            while cnt < 100 && !socket_path.exists() {
+                cnt += 1;
+                thread::sleep(Duration::from_millis(500))
+            }
+            if socket_path.exists() {
+                let listener = UnixListener::bind(tmpdir.path().join(FILENAME_OUT)).unwrap();
+
+                let mut socket = UnixStream::connect(tmpdir.path().join(FILENAME_IN)).unwrap();
+                socket.write_all(&input).unwrap();
+                // close the socket to let the test get EOF
+                drop(socket);
+
+                let (mut socket, _) = listener.accept().unwrap();
+
+                let mut buffer = Vec::new();
+                socket.read_to_end(&mut buffer).unwrap();
+
+                assert_eq_slices(&input, &buffer, "stream output");
+            }
+        }
+    });
+
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_unix_echo");
+    let input = tmpdir.path().as_os_str().as_bytes();
+    run_test(bin, 0, input, None, None);
+
+    handle.join().unwrap();
+}
+
+#[cfg(feature = "backend-sev")]
+#[test]
+#[cfg_attr(not(host_can_test_sev), ignore)]
+#[serial]
+fn rust_sev_attestation() {
+    let bin = env!("CARGO_BIN_FILE_SEV_ATTESTATION_sev_attestation");
+    run_test(bin, 0, None, None, None);
+}
+
+#[test]
+#[serial]
+fn memspike() {
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_memspike");
+    run_test(bin, 0, None, None, None);
+}
+
+#[test]
+#[serial]
+fn memory_stress_test() {
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_memory_stress_test");
+    run_test(bin, 0, None, None, None);
+}
+
+#[test]
+#[serial]
+fn cpuid() {
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_cpuid");
+    run_test(bin, 0, None, None, None);
+}
