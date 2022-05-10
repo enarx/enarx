9ff21daf11ca5f39700c0505ec52cc3bc7e79ff2
Roman Volosatovs <roman@profian.com>
2022-05-10 13:52:12 +0200

    test: trim the testing matrix
    Remove a few test cases to speed up tests, especially in CI
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- tests/wasm/mod.rs
+++ tests/wasm/mod.rs
@@ -4,7 +4,7 @@ use super::{check_output, CRATE, KEEP_BIN, OUT_DIR, TEST_BINS_OUT, TIMEOUT_SECS}
 
 use std::io::{stderr, Read, Write};
 use std::net::{Ipv4Addr, TcpListener, TcpStream};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::time;
 use std::{fs, thread};
@@ -13,17 +13,6 @@ use process_control::{ChildExt, Control, Output};
 use serial_test::serial;
 use tempfile::tempdir;
 use url::Url;
-
-fn create(path: &Path) {
-    match std::fs::create_dir(&path) {
-        Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {}
-        Err(e) => {
-            eprintln!("Can't create {:#?} : {:#?}", path, e);
-            std::process::exit(1);
-        }
-        Ok(_) => {}
-    }
-}
 
 fn enarx<'a>(
     cmd: impl FnOnce(&mut Command) -> &mut Command,
@@ -95,14 +84,18 @@ pub fn enarx_deploy<'a>(url: &Url, input: impl Into<Option<&'a [u8]>>) -> Output
     enarx(|cmd| cmd.arg("deploy").arg(url.as_str()), input)
 }
 
-fn compile(wasm: &str) {
-    let out_dir = Path::new(CRATE).join(OUT_DIR).join(TEST_BINS_OUT);
-    let wasm = out_dir.join(wasm);
+fn wasm_out() -> PathBuf {
+    Path::new(CRATE).join(OUT_DIR).join(TEST_BINS_OUT)
+}
 
-    create(&out_dir);
+fn wasm_path(wasm: &str) -> PathBuf {
+    wasm_out().join(wasm)
+}
+
+fn compile(wasm: &str) -> PathBuf {
+    let wasm = wasm_path(wasm);
 
     let src_path = &Path::new(CRATE).join("tests/crates/enarx_wasm_tests/wat");
-
     let wat = src_path
         .join(wasm.file_stem().unwrap())
         .with_extension("wat");
@@ -117,98 +110,22 @@ fn compile(wasm: &str) {
 
         if wasm_meta.len() > 0 && wasm_time > wat_time {
             // skip, if already compiled and newer than original
-            return;
+            return wasm;
         }
     }
 
-    let bin = wat::parse_file(&wat).unwrap_or_else(|_| panic!("failed to compile {:?}", &wat));
-    std::fs::write(&wasm, &bin).unwrap_or_else(|_| panic!("failed to write {:?}", &wasm));
-}
-
-const DEFAULT_CONFIG: &str = r#"[[files]]
-kind = "stdin"
-
-[[files]]
-kind = "stdout"
-
-[[files]]
-kind = "stderr""#;
-
-fn run_wasm_test<'a>(
-    wasm: &str,
-    conf: Option<&'a str>,
-    status: i32,
-    input: impl Into<Option<&'a [u8]>>,
-    expected_stdout: impl Into<Option<&'a [u8]>>,
-    expected_stderr: impl Into<Option<&'a [u8]>>,
-) {
-    let input = input.into();
-    let expected_stdout = expected_stdout.into();
-    let expected_stderr = expected_stderr.into();
-
-    let wasm = Path::new(CRATE)
-        .join(OUT_DIR)
-        .join(TEST_BINS_OUT)
-        .join(wasm);
-
-    if conf.is_none() {
-        check_output(
-            &enarx_run(&wasm, None, input),
-            status,
-            expected_stdout,
-            expected_stderr,
-        );
-
-        check_output(
-            &enarx_deploy(
-                &Url::from_file_path(&wasm).expect("failed to construct a URL from path"),
-                input,
-            ),
-            status,
-            expected_stdout,
-            expected_stderr,
-        );
-
-        // TODO: Test execution from a remote HTTP(S) URL
+    let out_dir = wasm_out();
+    match fs::create_dir(&out_dir) {
+        Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {}
+        Err(e) => {
+            eprintln!("Can't create {:#?} : {:#?}", out_dir, e);
+            std::process::exit(1);
+        }
+        Ok(_) => {}
     }
-
-    let pkg = tempdir().expect("failed to create temporary package directory");
-    let pkg_wasm = pkg.path().join("main.wasm");
-    let pkg_conf = pkg.path().join("Enarx.toml");
-
-    fs::copy(wasm, &pkg_wasm).expect("failed to copy WASM module");
-    fs::write(&pkg_conf, &conf.unwrap_or(DEFAULT_CONFIG)).expect("failed to write config");
-
-    check_output(
-        &enarx_run(pkg_wasm.as_path(), Some(pkg_conf.as_path()), input),
-        status,
-        expected_stdout,
-        expected_stderr,
-    );
-
-    check_output(
-        &enarx_deploy(
-            &Url::from_file_path(pkg.path()).expect("failed to construct a URL from package path"),
-            input,
-        ),
-        status,
-        expected_stdout,
-        expected_stderr,
-    )
-
-    // TODO: Test execution from a remote HTTP(S) URL
-}
-
-fn run_wat_test<'a>(
-    wasm: &str,
-    conf: Option<&'a str>,
-    status: i32,
-    input: impl Into<Option<&'a [u8]>>,
-    expected_stdout: impl Into<Option<&'a [u8]>>,
-    expected_stderr: impl Into<Option<&'a [u8]>>,
-) {
-    compile(wasm);
-    run_wasm_test(wasm, conf, status, input, expected_stdout, expected_stderr);
+    let bin = wat::parse_file(&wat).unwrap_or_else(|_| panic!("failed to compile {:?}", &wat));
+    fs::write(&wasm, &bin).unwrap_or_else(|_| panic!("failed to write {:?}", &wasm));
+    wasm
 }
 
 #[test]
@@ -217,7 +134,8 @@ fn return_1() {
     // This module does, in fact, return 1. But function return values
     // are separate from setting the process exit status code, so
     // we still expect a return code of '0' here.
-    run_wat_test("return_1.wasm", None, 0, None, None, None);
+    let wasm = compile("return_1.wasm");
+    check_output(&enarx_run(&wasm, None, None), 0, None, None);
 }
 
 #[test]
@@ -226,73 +144,79 @@ fn wasi_snapshot1() {
     // This module uses WASI to return the number of commandline args.
     // Since we don't currently do anything with the function return value,
     // we don't get any output here, and we expect '0', as above.
-    run_wat_test("wasi_snapshot1.wasm", None, 0, None, None, None);
+    let wasm = compile("wasi_snapshot1.wasm");
+    check_output(&enarx_run(&wasm, None, None), 0, None, None);
 }
 
 #[test]
 #[serial]
 fn hello_wasi_snapshot1() {
     // This module just prints "Hello, world!" to stdout. Hooray!
-    run_wat_test(
-        "hello_wasi_snapshot1.wasm",
-        None,
-        0,
-        None,
-        &b"Hello, world!\n"[..],
-        None,
-    );
+    let wasm = compile("hello_wasi_snapshot1.wasm");
+    const OUTPUT: &[u8] = br#"Hello, world!
+"#;
+    check_output(&enarx_run(&wasm, None, None), 0, OUTPUT, None);
 }
 
 #[test]
 #[serial]
 fn no_export() {
     // This module has no exported functions, so we get an error.
-    run_wat_test("no_export.wasm", None, 1, None, None, None);
+    let wasm = compile("no_export.wasm");
+    check_output(&enarx_run(&wasm, None, None), 1, None, None);
 }
 
 #[test]
 #[serial]
 fn echo() {
+    let wasm = wasm_path(env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_echo"));
+
     let mut input: Vec<_> = Vec::with_capacity(2 * 1024 * 1024);
     for i in 0..input.capacity() {
         input.push(i as _);
     }
     let input = input.as_slice();
-
-    const WASM: &str = env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_echo");
-    run_wasm_test(WASM, None, 0, input, input, None);
+    check_output(&enarx_run(&wasm, None, input), 0, input, None);
 }
 
 #[test]
 #[serial]
 fn memspike() {
-    const WASM: &str = env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_memspike");
-    run_wasm_test(WASM, None, 0, None, None, None);
+    let wasm = wasm_path(env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_memspike"));
+    check_output(&enarx_run(&wasm, None, None), 0, None, None);
 }
 
 #[test]
 #[serial]
 fn memory_stress_test() {
-    const WASM: &str = env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_memory_stress_test");
-    run_wasm_test(WASM, None, 0, None, None, None);
+    let wasm = wasm_path(env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_memory_stress_test"));
+    check_output(&enarx_run(&wasm, None, None), 0, None, None);
 }
 
 #[test]
 #[serial]
 fn zerooneone() {
-    const WASM: &str = env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_zerooneone");
-    const INPUT: &str = r#"Good morning, that's a nice tnetennba.
+    let wasm = wasm_path(env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_zerooneone"));
+    const INPUT: &[u8] = br#"Good morning, that's a nice tnetennba.
 0118 999 881 999 119 725 3
 "#;
-    const OUTPUT: &str = r#"Tbbq zbeavat, gung'f n avpr gargraaon.
+    const OUTPUT: &[u8] = br#"Tbbq zbeavat, gung'f n avpr gargraaon.
 0118 999 881 999 119 725 3
 "#;
-    run_wasm_test(WASM, None, 0, INPUT.as_bytes(), OUTPUT.as_bytes(), None);
+
+    check_output(&enarx_run(&wasm, None, INPUT), 0, OUTPUT, None);
+
+    let url = Url::from_file_path(&wasm).expect("failed to construct a URL from path");
+    check_output(&enarx_deploy(&url, INPUT), 0, OUTPUT, None);
+
+    // TODO: Test execution from a remote HTTP(S) URL
+    // https://github.com/enarx/enarx/issues/1855
 }
 
 #[test]
 #[serial]
 fn check_tcp() {
+    let wasm = wasm_path(env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_check_tcp"));
     const MSG: &str = r#"one
 two
 three
@@ -326,6 +250,17 @@ name = "CONNECT""#;
     let cport = connect.local_addr().unwrap().port();
     drop(connect);
 
+    let cfg = CFG
+        .replace("@@LPORT@@", &cport.to_string())
+        .replace("@@CPORT@@", &lport.to_string());
+
+    let pkg = tempdir().expect("failed to create temporary package directory");
+    let pkg_wasm = pkg.path().join("main.wasm");
+    let pkg_conf = pkg.path().join("Enarx.toml");
+
+    fs::copy(wasm, &pkg_wasm).expect("failed to copy WASM module");
+    fs::write(&pkg_conf, &cfg).expect("failed to write config");
+
     // Spawn the IO thread.
     thread::spawn(move || loop {
         let mut output = listen.accept().unwrap().0;
@@ -340,9 +275,11 @@ name = "CONNECT""#;
         assert_eq!(MSG, buffer)
     });
 
-    const WASM: &str = env!("CARGO_BIN_FILE_ENARX_WASM_TESTS_check_tcp");
-    let cfg = CFG
-        .replace("@@LPORT@@", &cport.to_string())
-        .replace("@@CPORT@@", &lport.to_string());
-    run_wasm_test(WASM, Some(cfg.as_str()), 0, None, None, None);
+    check_output(&enarx_run(&pkg_wasm, Some(&pkg_conf), None), 0, None, None);
+
+    let url = Url::from_file_path(&pkg).expect("failed to construct a URL from package path");
+    check_output(&enarx_deploy(&url, None), 0, None, None);
+
+    // TODO: Test execution from a remote HTTP(S) URL
+    // https://github.com/enarx/enarx/issues/1855
 }
