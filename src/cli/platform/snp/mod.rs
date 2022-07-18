4a5d8994b3a0c40f752d73994d143cb75df0282e
Harald Hoyer <harald@profian.com>
2022-07-18 09:56:25 +0200

    fix(sev): rename `enarx platform snp info`
    Rename `enarx platform snp info` to `enarx platform snp vcek` to reflect
    its real purpose. `enarx platform snp vcek --file` also sounds better.
    
    Also extend the description of `enarx platform snp update`.
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- src/cli/platform/snp/mod.rs
+++ src/cli/platform/snp/mod.rs
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
-mod info;
 mod update;
+mod vcek;
 
 use clap::Subcommand;
 
 /// SNP-specific functionality
 #[derive(Subcommand, Debug)]
 pub enum Subcommands {
-    Info(info::Options),
+    Vcek(vcek::Options),
     Update(update::Options),
 }
 
 impl Subcommands {
     pub fn dispatch(self) -> anyhow::Result<()> {
         match self {
-            Self::Info(cmd) => cmd.execute(),
+            Self::Vcek(cmd) => cmd.execute(),
             Self::Update(cmd) => cmd.execute(),
         }
     }
