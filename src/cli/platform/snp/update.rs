1a5737e1a0418430bba2695f795ee20834fc2adc
Harald Hoyer <harald@profian.com>
2022-07-18 09:56:25 +0200

    fix(sev): rename `enarx platform snp info`
    Rename `enarx platform snp info` to `enarx platform snp vcek` to reflect
    its real purpose. `enarx platform snp vcek --file` also sounds better.
    
    Also extend the description of `enarx platform snp update`.
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- src/cli/platform/snp/update.rs
+++ src/cli/platform/snp/update.rs
@@ -4,9 +4,8 @@ use crate::backend::sev::snp::vcek::vcek_write;
 
 use clap::Args;
 
-/// Download the VCEK certificate for this platform.
-///
-/// The certificate will be saved to a cache file in `/var/cache/amd-sev/`
+/// Download the current VCEK certificate for this platform
+/// to a cache file in the `/var/cache/amd-sev/` directory
 #[derive(Args, Debug)]
 pub struct Options {}
 
