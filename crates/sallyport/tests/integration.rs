cb09a1017d122c7d0df126727807a317e8246230
bstrie <865233+bstrie@users.noreply.github.com>
2022-07-20 05:08:49 -0400

    build: bump Rust toolchain to 2022-07-19
    Signed-off-by: bstrie <865233+bstrie@users.noreply.github.com>

--- crates/sallyport/tests/integration.rs
+++ crates/sallyport/tests/integration.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-#![cfg(all(target_arch = "x86_64", target_os = "linux"))]
+#![cfg(all(target_arch = "x86_64", target_os = "linux", not(miri)))]
 #![feature(c_size_t, core_ffi_c)]
 
 mod enarxcall;
