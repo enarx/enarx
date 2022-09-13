f182a6cb185204fc083e60d3a1b0cf2746496cfd
Harald Hoyer <harald@profian.com>
2022-09-13 14:09:23 +0200

    feat(tests): add threading tests
    Signed-off-by: Harald Hoyer <harald@profian.com>

---
+++ tests/crates/enarx_exec_tests/src/bin/futex.rs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use enarx_exec_tests::musl_fsbase_fix;
+
+use std::io;
+use std::sync::atomic::AtomicU32;
+use std::time;
+
+musl_fsbase_fix!();
+
+fn main() {
+    let futex = AtomicU32::new(0);
+    let futex_ptr = &futex as *const AtomicU32;
+    let timespec = libc::timespec {
+        tv_sec: 2,
+        tv_nsec: 0,
+    };
+
+    let now = time::Instant::now();
+    let r = unsafe {
+        libc::syscall(
+            libc::SYS_futex,
+            futex_ptr,
+            libc::FUTEX_WAIT | libc::FUTEX_PRIVATE_FLAG,
+            0,
+            &timespec as *const _,
+        )
+    };
+
+    assert_eq!(r, -1);
+    let err = io::Error::last_os_error().raw_os_error().unwrap();
+    assert_eq!(err, libc::ETIMEDOUT);
+
+    assert!(now.elapsed().as_secs() >= 2);
+}
--- tests/exec/mod.rs
+++ tests/exec/mod.rs
@@ -16,6 +16,73 @@ use tempfile::Builder;
 
 #[test]
 #[serial]
+#[cfg_attr(not(host_can_test_sgx), ignore = "Backend does not support SGX")]
+fn futex() {
+    if let Ok(backend) = std::env::var("ENARX_BACKEND") {
+        if backend != "sgx" {
+            return;
+        }
+    }
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_futex");
+
+    run_test(bin, 0, None, None, None);
+}
+
+#[test]
+#[serial]
+#[cfg_attr(not(host_can_test_sgx), ignore = "Backend does not support SGX")]
+fn thread() {
+    if let Ok(backend) = std::env::var("ENARX_BACKEND") {
+        if backend != "sgx" {
+            return;
+        }
+    }
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_thread");
+    let output = r#"Before Spawn
+After Spawn 1
+After Spawn 2
+Hello from Thread 2!
+Hello from Thread 1!
+After Join 1
+After Join 2
+"#;
+    run_test(bin, 0, None, output.as_bytes(), None);
+}
+
+#[test]
+#[serial]
+#[cfg_attr(not(host_can_test_sgx), ignore = "Backend does not support SGX")]
+fn thread_exit_group() {
+    if let Ok(backend) = std::env::var("ENARX_BACKEND") {
+        if backend != "sgx" {
+            return;
+        }
+    }
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_thread-exit-group");
+    let output = r#"Before Spawn
+After Spawn 1
+After Spawn 2
+Hello from Thread 2!
+"#;
+    run_test(bin, 0, None, output.as_bytes(), None);
+}
+
+#[test]
+#[serial]
+#[cfg_attr(not(host_can_test_sgx), ignore = "Backend does not support SGX")]
+fn thread_channel() {
+    if let Ok(backend) = std::env::var("ENARX_BACKEND") {
+        if backend != "sgx" {
+            return;
+        }
+    }
+    let bin = env!("CARGO_BIN_FILE_ENARX_EXEC_TESTS_thread-channel");
+    let output = "Start\nHello, thread\n";
+    run_test(bin, 0, None, output.as_bytes(), None);
+}
+
+#[test]
+#[serial]
 fn echo() {
     let mut input: Vec<u8> = Vec::with_capacity(2 * 1024 * 1024);
 
