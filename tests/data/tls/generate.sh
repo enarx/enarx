061988d14c2e312316ed0ffbdb3b4a1a0e44bcfa
Roman Volosatovs <roman@profian.com>
2022-09-22 12:14:15 +0200

    fix(test): prevent CA cert from expiring
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- tests/data/tls/generate.sh
+++ tests/data/tls/generate.sh
@@ -5,7 +5,7 @@ printf "\nCA "
 openssl pkey -noout -text -in ca.key
 
 printf "\nGenerating CA certificate\n"
-openssl req -new -x509 -config ca.conf -key ca.key -out ca.crt
+openssl req -new -x509 -days 9999 -config ca.conf -key ca.key -out ca.crt
 printf "\nCA "
 openssl x509 -noout -text -in ca.crt
 
