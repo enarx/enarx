83cd270f402e6e37a429e39adfea99a5d8c52192
Roman Volosatovs <roman@profian.com>
2022-10-07 20:47:35 +0200

    build(nix): switch to crane
    - `cargo fmt`, `cargo nextest` and `cargo clippy` are run as part of `nix flake check`
    - `static` package prefix is removed, instead users should specify
      "actual" target triples
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -1,30 +1,33 @@
 {
   description = "Tools for deploying WebAssembly into Enarx Keeps.";
 
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.fenix.url = github:nix-community/fenix;
+  inputs.crane.inputs.flake-compat.follows = "flake-compat";
+  inputs.crane.inputs.flake-utils.follows = "flake-utils";
+  inputs.crane.inputs.nixpkgs.follows = "nixpkgs";
+  # TODO: Switch to upstream once https://github.com/ipetkov/crane/pull/126 is merged
+  inputs.crane.url = github:rvolosatovs/crane/fix/no_std;
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.nixpkgs.url = github:profianinc/nixpkgs;
+  inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.rust-overlay.url = github:oxalica/rust-overlay;
 
   outputs = {
     self,
-    nixpkgs,
-    fenix,
+    crane,
     flake-utils,
+    nixpkgs,
+    rust-overlay,
     ...
   }:
-    with flake-utils.lib.system;
-      flake-utils.lib.eachSystem [
-        aarch64-darwin
-        aarch64-linux
-        x86_64-darwin
-        x86_64-linux
-      ]
-      (
-        system: let
-          ignorePatterns = [
+    with flake-utils.lib.system; let
+      version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+
+      overlay = final: prev: let
+        src =
+          final.nix-gitignore.gitignoreRecursiveSource [
             "*.nix"
             "*.yml"
             "/.github"
@@ -35,166 +38,148 @@
             "flake.lock"
             "LICENSE"
             "rust-toolchain.toml"
-          ];
+          ]
+          ./.;
 
-          pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-          cargo.toml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
+        craneLib = (crane.mkLib final).overrideToolchain rustToolchain;
 
-          buildPackage = targetPkgs: rustTargets: extraArgs: let
-            rust = with fenix.packages.${system};
-              combine (
-                [
-                  minimal.cargo
-                  minimal.rustc
-                ]
-                ++ map (target: targets.${target}.latest.rust-std) rustTargets
-              );
-          in
-            (targetPkgs.makeRustPlatform {
-              rustc = rust;
-              cargo = rust;
-            })
-            .buildRustPackage
-            (extraArgs
-              // {
-                inherit (cargo.toml.package) name version;
+        commonArgs = {
+          inherit
+            src
+            version
+            ;
+          pname = "enarx";
 
-                src = pkgs.nix-gitignore.gitignoreRecursiveSource ignorePatterns self;
+          buildInputs =
+            final.lib.optional final.stdenv.isDarwin
+            final.darwin.apple_sdk.frameworks.Security;
+        };
 
-                cargoLock.lockFileContents = builtins.readFile "${self}/Cargo.lock";
-                cargoLock.outputHashes."cranelift-bforest-0.89.0" = "sha256-ytUiONbe9pHceyNMtpjE2fRQF6SwO/rGXtDWQ3EkCK0="; # for Wasmtime 2.0.0 git rev
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs
+          // {
+            cargoExtraArgs = "-j $NIX_BUILD_CORES --all-features";
 
-                postPatch = ''
-                  patchShebangs ./helper
-                '';
+            # Remove binary dependency specification, since that breaks on generated "dummy source"
+            extraDummyScript = ''
+              sed -i '/^artifact = "bin"$/d' $out/Cargo.toml
+              sed -i '/^target = ".*"$/d' $out/Cargo.toml
+            '';
+          });
 
-                buildInputs =
-                  pkgs.lib.optional pkgs.stdenv.isDarwin
-                  pkgs.darwin.apple_sdk.frameworks.Security;
-              });
+        commonArtifactArgs = commonArgs // {inherit cargoArtifacts;};
 
-          dynamicBin =
-            buildPackage pkgs
-            ([
-                "wasm32-wasi" # required for tests
-              ]
-              ++ (
-                if system == aarch64-linux
-                then [
-                  "aarch64-unknown-linux-musl"
-                ]
-                else if system == x86_64-linux
-                then [
-                  "x86_64-unknown-linux-musl"
-                  "x86_64-unknown-none" # required for shims
-                ]
-                else []
-              ))
-            {
-              cargoTestFlags = ["wasm::"];
-            };
+        # TODO: Use `--workspace` once https://github.com/enarx/enarx/issues/2270 is resolved
+        #checks.clippy = craneLib.cargoClippy (commonArtifactArgs // {cargoClippyExtraArgs = "--all-targets --workspace -- --deny warnings";});
+        checks.clippy = craneLib.cargoClippy (commonArtifactArgs
+          // {
+            cargoExtraArgs = "-j $NIX_BUILD_CORES --all-features";
 
-          staticBin =
-            buildPackage pkgs.pkgsStatic
-            (
-              if system == aarch64-linux
-              then [
-                "aarch64-unknown-linux-musl"
-              ]
-              else if system == x86_64-linux
-              then [
-                "x86_64-unknown-linux-musl"
-                "x86_64-unknown-none" # required for shims
-              ]
-              else if (system == x86_64-darwin || system == aarch64-darwin)
-              then [
-                "wasm32-wasi" # required for tests
-              ]
-              else []
-            )
-            {
-              CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+        checks.fmt = craneLib.cargoFmt commonArgs;
 
-              depsBuildBuild = [
-                pkgs.stdenv.cc
-              ];
+        buildPackage = extraArgs:
+          craneLib.buildPackage (commonArtifactArgs
+            // {
+              cargoExtraArgs = "-j $NIX_BUILD_CORES";
+              cargoTestExtraArgs = "wasm::";
 
-              meta.mainProgram = cargo.toml.package.name;
-            };
+              installPhaseCommand = ''
+                mkdir -p $out/bin
+                cp target/''${CARGO_BUILD_TARGET:+''${CARGO_BUILD_TARGET}/}''${CARGO_PROFILE:-release}/enarx $out/bin/enarx
+              '';
+            }
+            // extraArgs);
 
-          aarch64DarwinBin =
-            if system == aarch64-darwin
-            then staticBin
-            else
-              # TODO: Support cross-compilation
-              throw "cross-compilation not supported, use QEMU";
+        nativeBin = buildPackage {};
+        aarch64DarwinBin = buildPackage {
+          CARGO_BUILD_TARGET = "aarch64-apple-darwin";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+        aarch64LinuxMuslBin = buildPackage {
+          CARGO_BUILD_TARGET = "aarch64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+        x86_64DarwinBin = buildPackage {
+          CARGO_BUILD_TARGET = "x86_64-apple-darwin";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+        x86_64LinuxMuslBin = buildPackage {
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
 
-          aarch64LinuxMuslBin =
-            if system == aarch64-linux
-            then staticBin
-            else
-              # TODO: Support cross-compilation
-              throw "cross-compilation not supported, use QEMU";
+        buildImage = bin:
+          final.dockerTools.buildImage {
+            name = "enarx";
+            tag = version;
+            contents = [
+              bin
+            ];
+            config.Cmd = ["enarx"];
+            config.Env = ["PATH=${bin}/bin"];
+          };
+      in {
+        enarx = nativeBin;
+        enarx-aarch64-apple-darwin = aarch64DarwinBin;
+        enarx-aarch64-apple-darwin-oci = buildImage aarch64DarwinBin;
+        enarx-aarch64-unknown-linux-musl = aarch64LinuxMuslBin;
+        enarx-aarch64-unknown-linux-musl-oci = buildImage aarch64LinuxMuslBin;
+        enarx-x86_64-apple-darwin = x86_64DarwinBin;
+        enarx-x86_64-apple-darwin-oci = buildImage x86_64DarwinBin;
+        enarx-x86_64-unknown-linux-musl = x86_64LinuxMuslBin;
+        enarx-x86_64-unknown-linux-musl-oci = buildImage x86_64LinuxMuslBin;
 
-          x86_64DarwinBin =
-            if system == x86_64-darwin
-            then staticBin
-            else
-              # TODO: Support cross-compilation
-              throw "cross-compilation not supported, use QEMU";
-
-          x86_64LinuxMuslBin =
-            if system == x86_64-linux
-            then staticBin
-            else
-              # TODO: Support cross-compilation
-              throw "cross-compilation not supported, use QEMU";
-
-          buildImage = bin:
-            pkgs.dockerTools.buildImage {
-              inherit (cargo.toml.package) name;
-              tag = cargo.toml.package.version;
-              contents = [
-                bin
-              ];
-              config.Cmd = [cargo.toml.package.name];
-              config.Env = ["PATH=${bin}/bin"];
-            };
+        enarxChecks = checks;
+        enarxRustToolchain = rustToolchain;
+      };
+    in
+      {
+        overlays.default = overlay;
+      }
+      // flake-utils.lib.eachSystem [
+        aarch64-darwin
+        aarch64-linux
+        powerpc64le-linux
+        x86_64-darwin
+        x86_64-linux
+      ]
+      (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              # NOTE: Order is important
+              rust-overlay.overlays.default
+              overlay
+            ];
+          };
         in {
           formatter = pkgs.alejandra;
 
+          checks = pkgs.enarxChecks;
+
           packages =
             {
-              default = dynamicBin;
-
-              "${cargo.toml.package.name}" = dynamicBin;
-              "${cargo.toml.package.name}-static" = staticBin;
-              "${cargo.toml.package.name}-static-oci" = buildImage staticBin;
+              default = pkgs.enarx;
             }
-            // pkgs.lib.optionalAttrs (system == aarch64-darwin) {
-              "${cargo.toml.package.name}-aarch64-apple-darwin" = aarch64DarwinBin;
-              "${cargo.toml.package.name}-aarch64-apple-darwin-oci" = buildImage aarch64DarwinBin;
-            }
-            // pkgs.lib.optionalAttrs (system == aarch64-linux) {
-              "${cargo.toml.package.name}-aarch64-unknown-linux-musl" = aarch64LinuxMuslBin;
-              "${cargo.toml.package.name}-aarch64-unknown-linux-musl-oci" = buildImage aarch64LinuxMuslBin;
-            }
-            // pkgs.lib.optionalAttrs (system == x86_64-darwin) {
-              "${cargo.toml.package.name}-x86_64-apple-darwin" = x86_64DarwinBin;
-              "${cargo.toml.package.name}-x86_64-apple-darwin-oci" = buildImage x86_64DarwinBin;
-            }
-            // pkgs.lib.optionalAttrs (system == x86_64-linux) {
-              "${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
-              "${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
-            };
+            // pkgs.lib.genAttrs [
+              "enarx"
+              "enarx-aarch64-apple-darwin"
+              "enarx-aarch64-apple-darwin-oci"
+              "enarx-aarch64-unknown-linux-musl"
+              "enarx-aarch64-unknown-linux-musl-oci"
+              "enarx-x86_64-apple-darwin"
+              "enarx-x86_64-apple-darwin-oci"
+              "enarx-x86_64-unknown-linux-musl"
+              "enarx-x86_64-unknown-linux-musl-oci"
+            ] (name: pkgs.${name});
 
           devShells.default = pkgs.mkShell {
             buildInputs = [
-              (fenix.packages.${system}.fromToolchainFile {
-                file = "${self}/rust-toolchain.toml";
-              })
-
+              pkgs.enarxRustToolchain
               pkgs.openssl
             ];
           };
