4e472654f4c77b9743abe037454663a9f351d4c0
Roman Volosatovs <roman@profian.com>
2022-10-10 21:28:01 +0200

    build: support cross-compilation in `build.rs`
    `target_os` and `target_arch` are always the host triple in `build.rs`,
    which may or may not correspond to Enarx compilation target.
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- build.rs
+++ build.rs
@@ -1,14 +1,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use once_cell::sync::Lazy;
+use std::env::var;
+use std::fs;
+use std::path::{Path, PathBuf};
+
+/// Whether Enarx compilation target is x86_64 Linux or not.
+// NOTE: This may or may not correspond to `target_os` and `target_arch`, since `build.rs` is
+// compiled for the host triple and not the cargo compilation target
+static IS_X86_64_LINUX: Lazy<bool> = Lazy::new(|| {
+    var("CARGO_CFG_TARGET_OS").expect("missing CARGO_CFG_TARGET_OS") == "linux"
+        && var("CARGO_CFG_TARGET_ARCH").expect("missing CARGO_CFG_TARGET_ARCH") == "x86_64"
+});
+
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 use std::os::unix::fs::FileTypeExt;
 
-#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 fn generate_protos() {
-    use std::path::{Path, PathBuf};
-
     fn create(path: &Path) {
-        match std::fs::create_dir(path) {
+        match fs::create_dir(path) {
             Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {}
             Err(e) => {
                 eprintln!("Can't create {:#?} : {:#?}", path, e);
@@ -18,7 +28,7 @@ fn generate_protos() {
         }
     }
 
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(var("OUT_DIR").unwrap());
     let out_dir_proto = out_dir.join("protos");
     create(&out_dir_proto);
 
@@ -39,15 +49,17 @@ fn main() {
     // FIXME: this exists to work around https://github.com/rust-lang/cargo/issues/10527
     println!("cargo:rerun-if-changed=crates/");
 
-    #[cfg(all(target_os = "linux", target_arch = "x86_64",))]
-    println!("cargo:rustc-cfg=enarx_with_shim");
+    if *IS_X86_64_LINUX {
+        println!("cargo:rustc-cfg=enarx_with_shim");
+    }
+
+    if *IS_X86_64_LINUX {
+        generate_protos();
+    }
 
     #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-    generate_protos();
-
-    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-    if std::path::Path::new("/dev/sgx_enclave").exists()
-        && std::fs::metadata("/dev/sgx_enclave")
+    if Path::new("/dev/sgx_enclave").exists()
+        && fs::metadata("/dev/sgx_enclave")
             .unwrap()
             .file_type()
             .is_char_device()
@@ -57,20 +69,17 @@ fn main() {
         println!("cargo:rustc-cfg=host_can_test_sgx");
 
         if (!cfg!(feature = "disable-sgx-attestation"))
-            && std::path::Path::new(AESM_SOCKET).exists()
-            && std::fs::metadata(AESM_SOCKET)
-                .unwrap()
-                .file_type()
-                .is_socket()
+            && Path::new(AESM_SOCKET).exists()
+            && fs::metadata(AESM_SOCKET).unwrap().file_type().is_socket()
         {
             println!("cargo:rustc-cfg=host_can_test_attestation");
         }
     }
 
     #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-    if std::path::Path::new("/dev/sev").exists() {
+    if Path::new("/dev/sev").exists() {
         // Not expected to fail, as the file exists.
-        let metadata = std::fs::metadata("/dev/sev").unwrap();
+        let metadata = fs::metadata("/dev/sev").unwrap();
         let file_type = metadata.file_type();
 
         if file_type.is_char_device() {
