e2ad16564b3e726562d3dc4936e34fa0262af402
Roman Volosatovs <roman@profian.com>
2022-12-22 03:28:52 +0100

    test: only execute `unix_echo` on Linux
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- tests/crates/enarx_exec_tests/src/bin/unix_echo.rs
+++ tests/crates/enarx_exec_tests/src/bin/unix_echo.rs
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use enarx_exec_tests::musl_fsbase_fix;
-use std::io::{self, stdin, Read, Write};
-use std::os::unix::net::{UnixListener, UnixStream};
-use std::path::PathBuf;
 
 musl_fsbase_fix!();
 
-fn main() -> io::Result<()> {
+#[cfg(unix)]
+fn main() -> std::io::Result<()> {
+    use std::io::{stdin, Read, Write};
+    use std::os::unix::net::{UnixListener, UnixStream};
+    use std::path::PathBuf;
+
     let mut dir_name = String::new();
 
     stdin().read_line(&mut dir_name)?;
@@ -23,4 +25,9 @@ fn main() -> io::Result<()> {
     let mut socket = UnixStream::connect(dir_name.join("enarx_unix_echo_from_bin")).unwrap();
     socket.write_all(&buffer)?;
     Ok(())
+}
+
+#[cfg(not(unix))]
+fn main() {
+    panic!("only supported on Unix")
 }
