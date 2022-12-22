454369163ed1d9286adc2285eedd5dfaba57593d
Harald Hoyer <harald@profian.com>
2022-12-22 09:07:53 +0100

    chore(shims): cleanup shim main.rs
    Same attributes, comments, etc.
    
    Signed-off-by: Harald Hoyer <harald@profian.com>

--- crates/shim-kvm/src/main.rs
+++ crates/shim-kvm/src/main.rs
@@ -1,18 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! The SEV shim
-//!
-//! Contains the startup code and the main function.
+//! The SEV-SNP and KVM shim executable
+
+#![feature(asm_const)]
 #![deny(clippy::all)]
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![feature(asm_const)]
 #![cfg_attr(coverage, feature(no_coverage))]
 #![cfg_attr(target_os = "none", no_std)]
 #![cfg_attr(target_os = "none", no_main)]
 
-#[cfg(target_os = "none")]
+#[cfg(all(
+    target_arch = "x86_64",
+    target_vendor = "unknown",
+    target_os = "none",
+    target_env = ""
+))]
 pub mod start;
 
-#[cfg(not(target_os = "none"))]
+/// A fake main function
+///
+/// This is used when compiling the shim for the host.
+/// Look into the [start] module for the real main function.
+#[cfg(not(all(
+    target_arch = "x86_64",
+    target_vendor = "unknown",
+    target_os = "none",
+    target_env = ""
+)))]
 pub fn main() {}
--- crates/shim-sgx/src/main.rs
+++ crates/shim-sgx/src/main.rs
@@ -1,19 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
 
-//! The SGX shim
-//!
-//! This crate contains the system that traps the syscalls (and cpuid
-//! instructions) from the enclave code and proxies them to the host.
+//! The SGX shim executable
 
 #![feature(asm_const)]
 #![deny(clippy::all)]
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
+#![cfg_attr(coverage, feature(no_coverage))]
 #![cfg_attr(target_os = "none", no_std)]
 #![cfg_attr(target_os = "none", no_main)]
 
-#[cfg(not(target_os = "none"))]
-pub fn main() {}
-
-#[cfg(target_os = "none")]
+#[cfg(all(
+    target_arch = "x86_64",
+    target_vendor = "unknown",
+    target_os = "none",
+    target_env = ""
+))]
 pub mod start;
+
+/// A fake main function
+///
+/// This is used when compiling the shim for the host.
+/// Look into the [start] module for the real main function.
+#[cfg(not(all(
+    target_arch = "x86_64",
+    target_vendor = "unknown",
+    target_os = "none",
+    target_env = ""
+)))]
+pub fn main() {}
