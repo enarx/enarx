461799bdbeb58a26865eab76ad29a51876336458
Tom Dohrmann <erbse.13@gmx.de>
2024-06-26 12:03:04 +0000

    feat(snp): handle hypercall instead of vmgexit
    Previously when the keep issued a page state request, the kernel
    forwarded that request to userspace which has to decode and handle
    that request. With the latest SEV-SNP host patches (v15), the kernel
    decodes the PSCs and only asks userspace to handle it by pretending the
    keep issued a hypercall.
    
    Signed-off-by: Tom Dohrmann <erbse.13@gmx.de>

--- src/backend/kvm/thread.rs
+++ src/backend/kvm/thread.rs
@@ -3,13 +3,12 @@
 use super::super::Command;
 use super::Keep;
 use super::KeepPersonality;
+use super::KVM_HC_MAP_GPA_RANGE;
 #[cfg(feature = "gdb")]
 use crate::backend::execute_gdb;
 use crate::backend::kvm::builder::kvm_new_vcpu;
 use crate::backend::parking::THREAD_PARK;
 use crate::backend::sev::set_memory_attributes;
-use crate::backend::sev::snp::ghcb::Ghcb;
-use crate::backend::sev::snp::ghcb::SnpPscDesc;
 use crate::backend::Keep as _;
 
 use std::io;
@@ -19,8 +18,9 @@ use std::ops::DerefMut;
 use std::os::fd::AsRawFd;
 use std::sync::{Arc, RwLock};
 
-use anyhow::{anyhow, bail, ensure, Context, Result};
-use kvm_bindings::kvm_run__bindgen_ty_1;
+use anyhow::ensure;
+use anyhow::{bail, Context, Result};
+use kvm_ioctls::HypercallExit;
 use kvm_ioctls::{VcpuExit, VcpuFd};
 use libc::{
     c_int, fallocate, madvise, timespec, FALLOC_FL_KEEP_SIZE, FALLOC_FL_PUNCH_HOLE, MADV_FREE,
@@ -32,7 +32,7 @@ use sallyport::item::enarxcall::Payload;
 use sallyport::item::{Block, Item};
 use sallyport::libc::EAGAIN;
 use sallyport::{item, KVM_SYSCALL_TRIGGER_EXIT_THREAD, KVM_SYSCALL_TRIGGER_PORT};
-use tracing::{error, trace, trace_span, warn};
+use tracing::{error, trace, trace_span};
 use x86_64::PhysAddr;
 
 pub struct Thread<P: KeepPersonality> {
@@ -279,198 +279,17 @@ impl<P: KeepPersonality> Thread<P> {
             -EAGAIN as usize
         }
     }
-
-    fn handle_vmgexit(&mut self, ghcb_msr: &mut u64, _error: &mut u8) -> Result<()> {
-        match *ghcb_msr & 0xfff {
-            0x000 => {
-                // GHCB Guest Physical Address
-
-                self.handle_ghcb_request(*ghcb_msr)?;
-            }
-            0x014 => {
-                // SNP Page State Change Request
-
-                self.handle_msr_page_state_request(ghcb_msr)?;
-            }
-            f => bail!("unimplemented GHCB protocol function {f:#03x}"),
-        }
-
-        Ok(())
-    }
-
-    fn handle_ghcb_request(&mut self, ghcb_msr: u64) -> Result<(), anyhow::Error> {
-        let gpa = ghcb_msr & !0xfff;
-        let gpa = PhysAddr::new(gpa);
-
-        let mut guard = self.keep.write().unwrap();
-        let keep = &mut *guard;
-
-        // Find the memory slot that backs the guest physical address of the
-        // GHCB.
-        let region = keep
-            .regions
-            .iter_mut()
-            .find(|region| region.as_guest().contains(&gpa))
-            .context("can't find GHCB")?;
-        let offset = usize::try_from(gpa - region.as_guest().start).unwrap();
-
-        // Create a reference to the GHCB.
-        let ghcb_slice = &mut region.backing_mut()[offset..][..0x1000];
-        let ghcb = unsafe {
-            // SAFETY: `Ghcb` is a 0x1000 byte sized struct that's valid for
-            // all bit patterns and has no padding bytes.
-            // We assume that the guest passes us a unique reference to the
-            // memory.
-            &mut *(ghcb_slice as *mut [u8] as *mut Ghcb)
-        };
-
-        // Validate ghcb protocol.
-        ensure!(ghcb.ghcb_usage == 0);
-        ensure!(ghcb.protocol_version <= 2);
-
-        match ghcb.save_area.sw_exit_code {
-            0x8000_0010 => {
-                // SNP Page Stage Change
-
-                // Make sure that the page state change struct is in the shared
-                // buffer.
-                // The GHCB spec suggests this, but doesn't require it.
-                // However, our guest implementation always uses the shared
-                // buffer and using that knowledge allows us to simplify the
-                // code.
-                ensure!(
-                    ghcb.save_area.sw_scratch == gpa.as_u64() + 2048,
-                    "the page state change struct is not in the shared buffer"
-                );
-
-                // Create a reference to the page state change struct in the
-                // shared buffer.
-                let psc_desc = unsafe {
-                    // SAFETY: `SnpPscDesc` is a 2032 byte sized struct that's
-                    // valid for all bit patterns and has no padding bytes.
-                    &mut *(&mut ghcb.shared_buffer as *mut [u8; 2032] as *mut SnpPscDesc)
-                };
-
-                while psc_desc.cur_entry <= psc_desc.end_entry {
-                    // Process a page state change.
-                    let res = handle_next_page_state_change_request(psc_desc, keep);
-
-                    // Handle the result.
-                    match res {
-                        Ok(_) => {
-                            psc_desc.cur_entry += 1;
-                        }
-                        Err(error_code) => {
-                            ghcb.save_area.sw_exit_info2 = error_code;
-                            break;
-                        }
-                    }
-                }
-            }
-            _ => {
-                bail!("unimplemented sw_exit_code {:#x}", {
-                    ghcb.save_area.sw_exit_code
-                })
-            }
-        }
-
-        Ok(())
-    }
-
-    fn handle_msr_page_state_request(&mut self, ghcb_msr: &mut u64) -> Result<()> {
-        let gpa = *ghcb_msr & 0x7_ffff_ffff_f000;
-        let page_operation = (*ghcb_msr >> 52) & 0xf;
-
-        match page_operation {
-            1 => {
-                // Page assignment, Private
-
-                execute_page_state_change(gpa, &mut self.keep.write().unwrap(), true)
-                    .map_err(|_| anyhow!("failed to change page state to private"))?;
-            }
-            2 => {
-                // Page assignment, Shared
-
-                execute_page_state_change(gpa, &mut self.keep.write().unwrap(), false)
-                    .map_err(|_| anyhow!("failed to change page state to shared"))?;
-            }
-            _ => bail!("unimplemented operation {page_operation:#x}"),
-        }
-
-        *ghcb_msr = 0x015; // Page State Change Response - Success
-
-        Ok(())
-    }
-}
-
-const INVALID_HEADER_ERROR: u64 = 0x0000_0001_0000_0001;
-const INVALID_ENTRY_ERROR: u64 = 0x0000_0001_0000_0002;
-const UNSPECIFIED_ERROR: u64 = 0x0000_0100_0000_0000;
-
-fn handle_next_page_state_change_request<P>(
-    psc_desc: &SnpPscDesc,
-    keep: &mut Keep<P>,
-) -> Result<(), u64>
-where
-    P: KeepPersonality,
-{
-    let entry = psc_desc
-        .entries
-        .get(usize::from(psc_desc.cur_entry))
-        .ok_or(INVALID_HEADER_ERROR)?;
-    let cur_page = entry.entry & 0xfff;
-    let gpa = entry.entry & 0x7_ffff_ffff_f000;
-    let operation = (entry.entry >> 52) & 0xf;
-    let page_size = (entry.entry >> 56) & 1;
-
-    // Check that the guest requested page state change for a
-    // 4KiB page. We never map 2MiB pages into the guest, so
-    // there's no reason for the guest to request anything else
-    // and for us to support anything else.
-    if page_size != 0 {
-        return Err(UNSPECIFIED_ERROR);
-    }
-    if cur_page != 0 {
-        return Err(INVALID_ENTRY_ERROR);
-    }
-
-    // Try to execute the request.
-    match operation {
-        0x001 => {
-            // Page assignment, Private
-
-            execute_page_state_change(gpa, keep, true)
-        }
-        0x002 => {
-            // Page assignment, Shared
-
-            execute_page_state_change(gpa, keep, false)
-        }
-        0x003 => {
-            // PSMASH hint
-
-            // We're not required to process the hint.
-            Ok(())
-        }
-        0x004 => {
-            // UNSMASH hint
-
-            // We're not required to process the hint.
-            Ok(())
-        }
-        _ => {
-            warn!("unimplemented page state change operation {operation:#x}");
-
-            // Indicate to the guest that the entry is not valid.
-            Err(INVALID_ENTRY_ERROR)
-        }
-    }
 }
 
 /// Execute a page state change.
 ///
 /// This will also free up the memory in the opposite mapping.
-fn execute_page_state_change<P>(gpa: u64, keep: &mut Keep<P>, private: bool) -> Result<(), u64>
+fn execute_page_state_change<P>(
+    gpa: u64,
+    num_pages: u64,
+    keep: &mut Keep<P>,
+    private: bool,
+) -> Result<()>
 where
     P: KeepPersonality,
 {
@@ -481,7 +300,7 @@ where
         .regions
         .iter()
         .find(|r| r.as_guest().contains(&gpa))
-        .ok_or(UNSPECIFIED_ERROR)?;
+        .context("Couldn't find memory region")?;
 
     let offset = gpa - region.as_guest().start;
 
@@ -497,27 +316,21 @@ where
             region.restricted_fd().unwrap().as_raw_fd(),
             mode,
             offset as i64,
-            0x1000,
+            num_pages as i64 * 0x1000,
         )
     };
-    if res != 0 {
-        return Err(UNSPECIFIED_ERROR);
-    }
+    ensure!(res == 0, "fallocate call failed");
 
     // Switch the memory.
-    set_memory_attributes(&mut keep.vm_fd, gpa.as_u64(), 0x1000, private).map_err(|_| {
-        // Indicate to the guest that an unspecified error occured.
-        UNSPECIFIED_ERROR
-    })?;
+    set_memory_attributes(&mut keep.vm_fd, gpa.as_u64(), num_pages * 0x1000, private)
+        .context("Failed to update memory attributes")?;
 
     if private {
         // Tell the kernel that we don't need the shared mapping right now.
         let addr = region.as_virt().start + offset;
         let addr = addr.as_mut_ptr();
-        let res = unsafe { madvise(addr, 0x1000, MADV_FREE) };
-        if res != 0 {
-            return Err(UNSPECIFIED_ERROR);
-        }
+        let res = unsafe { madvise(addr, num_pages as usize * 0x1000, MADV_FREE) };
+        ensure!(res == 0, "Failed to free memory in shared mapping");
     }
 
     Ok(())
@@ -620,33 +433,23 @@ impl<P: KeepPersonality> super::super::Thread for Thread<P> {
                 self.keep.write().unwrap().sallyports[block_nr].replace(block_virt);
                 Ok(Command::Continue)
             }
-            VcpuExit::Unsupported(50) => {
-                // Vmgexit
-
-                // Cast the exit to a KvmExitVmgexit structure
-                // FIXME: Move this into kvm-ioctls once the SEV-SNP host
-                // patches land upstream.
-                #[repr(C, packed)]
-                pub struct KvmExitVmgexit {
-                    pub ghcb_msr: u64,
-                    pub error: u8,
+            VcpuExit::Hypercall(HypercallExit {
+                nr: KVM_HC_MAP_GPA_RANGE,
+                args: [address, num_pages, attrs, ..],
+                ret,
+                ..
+            }) => {
+                let private = (attrs & (1 << 3)) != 0;
+                let res = execute_page_state_change(
+                    address,
+                    num_pages,
+                    &mut *self.keep.write().unwrap(),
+                    private,
+                );
+                match res {
+                    Ok(_) => *ret = 0,
+                    Err(_) => *ret = 1,
                 }
-                let exit = &mut vcpu_fd.get_kvm_run().__bindgen_anon_1;
-                let vmgexit = unsafe {
-                    // SAFETY: `KvmExitVmgexit` has no alignment or bit-pattern requirements.
-                    &mut *(exit as *mut kvm_run__bindgen_ty_1 as *mut KvmExitVmgexit)
-                };
-
-                // Move from packed struct into normally aligned variables on the stack.
-                let mut ghcb_msr = vmgexit.ghcb_msr;
-                let mut error = vmgexit.error;
-
-                self.handle_vmgexit(&mut ghcb_msr, &mut error)?;
-
-                // Move back from the stack.
-                vmgexit.ghcb_msr = ghcb_msr;
-                vmgexit.error = error;
-
                 Ok(Command::Continue)
             }
             #[cfg(debug_assertions)]
